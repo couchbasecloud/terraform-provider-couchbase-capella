5d40cf0dfd42864576961208d252ad81d4851fab
Matty Maclean <matty.maclean@couchbase.com>
2023-09-14 14:16:05 +0100

    Add docstrings to client

--- internal/api/client.go
+++ internal/api/client.go
@@ -8,10 +8,12 @@ import (
 	"time"
 )
 
+// Client is responsible for constructing and executing HTTP requests.
 type Client struct {
 	*http.Client
 }
 
+// NewClient instantiates a new Client.
 func NewClient(timeout time.Duration) *Client {
 	return &Client{
 		Client: &http.Client{
@@ -20,11 +22,14 @@ func NewClient(timeout time.Duration) *Client {
 	}
 }
 
+// Reponse stuct is used to encapsulate the response details
 type Response struct {
 	Response *http.Response
 	Body     []byte
 }
 
+// Execute is used to construct and execute a HTTP request.
+// It then returns the response.
 func (c *Client) Execute(url string, method string, payload io.Reader, apiToken string) (response *Response, err error) {
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
