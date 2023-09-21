aa935c02b8100aff6ec1d3ebcdfdd9036a12e706
matty271828 <65253959+matty271828@users.noreply.github.com>
2023-09-21 18:05:27 +0100

    [AV-62076] - Add requires replacement to plan modifiers (#16)
    * Add requires replacement to plan modifiers
    
    * Add comment to explain blank update logic
    
    Open-api does not support PUT. Terraform will therefore
    default to executing a delete followed by a create.

--- internal/resources/allowlists.go
+++ internal/resources/allowlists.go
@@ -190,7 +190,13 @@ func (r *AllowList) Read(ctx context.Context, req resource.ReadRequest, resp *re
 
 // Update updates the allowlist.
 func (r *AllowList) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	// todo
+	// Couchbase Capella's v4 does not support a PUT endpoint for allowlists.
+	// Allowlists can only be created, read and deleted.
+	// http://cbc-cp-api.s3-website-us-east-1.amazonaws.com/#tag/allowedCIDRs(Cluster)
+	//
+	// Note: In this situation, terraform apply will default to deleting and executing a new create.
+	// The update implementation should simply be left empty.
+	// https://developer.hashicorp.com/terraform/plugin/framework/resources/update
 }
 
 // Delete deletes the allow list.
