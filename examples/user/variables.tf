86427d88fb2d647789aba93c4decdf9dcb2a4837
Matty Maclean <matty.maclean@couchbase.com>
2023-09-27 17:05:56 +0100

    Add example directory for user resource

---
+++ examples/user/variables.tf
@@ -0,0 +1,16 @@
+variable "host" {
+  description = "The Host URL of Couchbase Cloud."
+}
+
+variable "organization_id" {
+  description = "Capella Organization ID"
+}
+
+variable "project_id" {
+  description = "Capella Project ID"
+}
+
+variable "auth_token" {
+  description = "Authentication API Key"
+  sensitive   = true
+}
