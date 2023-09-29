501c895dc442090ac8cfb16c204af3f05e3b32fb
Matty Maclean <matty.maclean@couchbase.com>
2023-09-29 16:54:20 +0100

    Add string conversion of user resources

--- internal/resources/user.go
+++ internal/resources/user.go
@@ -2,11 +2,16 @@ package resources
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"terraform-provider-capella/internal/api"
 	providerschema "terraform-provider-capella/internal/schema"
 
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
@@ -54,7 +59,75 @@ func (r *User) Configure(ctx context.Context, req resource.ConfigureRequest, res
 
 // Create creates a new user
 func (r *User) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	// todo (AV-69624):
+	var plan providerschema.User
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	createUserRequest := api.CreateUserRequest{
+		Name:              plan.Name.ValueString(),
+		Email:             plan.Email.ValueString(),
+		OrganizationRoles: r.convertOrganizationRoles(plan.OrganizationRoles),
+		Resources:         r.convertResources(plan.Resources),
+	}
+
+	// Execute request
+	response, err := r.Client.Execute(
+		fmt.Sprintf(
+			"%s/v4/organizations/%s/users",
+			r.HostURL,
+			plan.OrganizationId.ValueString(),
+		),
+		http.MethodPost,
+		createUserRequest,
+		r.Token,
+		nil,
+	)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error executing request",
+			"Could not execute request, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	createUserResponse := api.CreateUserResponse{}
+	err = json.Unmarshal(response.Body, &createUserResponse)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating user",
+			"Could not create user, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	refreshedState, err := r.refreshUser(ctx, plan.OrganizationId.String(), plan.Id.String())
+	switch err := err.(type) {
+	case nil:
+	case api.Error:
+		resp.Diagnostics.AddError(
+			"Error reading Capella User",
+			"Could not read Capella User "+createUserResponse.Id.String()+": "+err.CompleteError(),
+		)
+		return
+	default:
+		resp.Diagnostics.AddError(
+			"Error reading Capella User",
+			"Could not read Capella User "+createUserResponse.Id.String()+": "+err.Error(),
+		)
+		return
+	}
+
+	// Set state to fully populated data
+	diags = resp.State.Set(ctx, refreshedState)
+
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 // Read reads user information
@@ -70,6 +143,78 @@ func (r *User) Update(ctx context.Context, req resource.UpdateRequest, resp *res
 // Delete deletes the user
 func (r *User) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	// todo (AV-69627):
+}
+
+// convertOrganizationRoles is used to convert all roles
+// in an array of basetypes.StringValue to strings.
+func (r *User) convertOrganizationRoles(organizationRoles []basetypes.StringValue) []string {
+	var convertedRoles []string
+	for _, role := range organizationRoles {
+		convertedRoles = append(convertedRoles, role.ValueString())
+	}
+	return convertedRoles
+}
+
+// convertResource is used to convert a resource object containing nested fields
+// of type basetypes.StringValue to a resource object containing nested fields of type string.
+func (r *User) convertResources(resources []providerschema.Resource) []api.Resource {
+	var convertedResources []api.Resource
+	for _, resource := range resources {
+		var convertedResource api.Resource
+		convertedResource.Id = resource.Id.ValueString()
+		convertedResource.Type = resource.Type.ValueString()
+
+		// Iterate through roles belonging to the user and convert to string
+		var convertedRoles []string
+		for _, role := range resource.Roles {
+			convertedRoles = append(convertedRoles, role.ValueString())
+		}
+		convertedResource.Roles = convertedRoles
+
+		convertedResources = append(convertedResources, convertedResource)
+	}
+	return convertedResources
+}
+
+// getUser is used to retrieve an existing user
+func (r *User) getUser(ctx context.Context, organizationId, userId string) (*api.GetUserResponse, error) {
+	response, err := r.Client.Execute(
+		fmt.Sprintf(
+			"%s/v4/organizations/%s/userss/%s",
+			r.HostURL,
+			organizationId,
+			userId,
+		),
+		http.MethodGet,
+		nil,
+		r.Token,
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error executing request: %s", err)
+	}
+
+	userResp := api.GetUserResponse{}
+	err = json.Unmarshal(response.Body, &userResp)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %s", err)
+	}
+	return &userResp, nil
+}
+
+func (r *User) refreshUser(ctx context.Context, organizationId, userId string) (*providerschema.OneUser, error) {
+	userResp, err := r.getUser(ctx, organizationId, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Populate fields in refreshedstate
+	refreshedState := providerschema.OneUser{
+		Id:             types.StringValue(userResp.Id.String()),
+		OrganizationId: types.StringValue(organizationId),
+	}
+
+	return &refreshedState, nil
 }
 
 // ImportState imports a remote user that was not created by Terraform.
