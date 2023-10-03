fa79ac7b3160f7b3cb4ef25b674f89762658171d
Nidhi Kumar <nidhi.kumar@couchbase.com>
2023-10-02 22:34:03 -0700

    added docstring

--- internal/api/certificate.go
+++ internal/api/certificate.go
@@ -1,5 +1,7 @@
 package api
 
+// GetCertificateResponse defines model for GetCertificateResponse.
 type GetCertificateResponse struct {
+	// Certificate is the certificate of the capella cluster
 	Certificate string `json:"certificate"`
 }
