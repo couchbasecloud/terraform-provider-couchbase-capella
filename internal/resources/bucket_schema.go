50edd01c0170b9cadc719bfd692f134bfe576f2a
Nidhi Kumar <nidhi.kumar@couchbase.com>
2023-10-31 21:18:28 -0700

    updated bucket schema

--- internal/resources/bucket_schema.go
+++ internal/resources/bucket_schema.go
@@ -2,7 +2,10 @@ package resources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
@@ -15,26 +18,93 @@ func BucketSchema() schema.Schema {
 					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
-			"organization_id":            stringAttribute(required),
-			"project_id":                 stringAttribute(required),
-			"cluster_id":                 stringAttribute(required),
-			"name":                       stringAttribute(required, requiresReplace),
-			"type":                       stringAttribute(optional, computed),
-			"storage_backend":            stringAttribute(optional, computed),
-			"memory_allocation_in_mb":    int64Attribute(optional, computed),
-			"bucket_conflict_resolution": stringAttribute(optional, computed),
-			"durability_level":           stringAttribute(optional, computed),
-			"replicas":                   int64Attribute(optional, computed),
-			"flush":                      boolAttribute(optional, computed),
-			"time_to_live_in_seconds":    int64Attribute(optional, computed),
-			"eviction_policy":            stringAttribute(computed, optional),
+			"name": schema.StringAttribute{
+				Required: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"organization_id": schema.StringAttribute{
+				Required: true,
+			},
+			"project_id": schema.StringAttribute{
+				Required: true,
+			},
+			"cluster_id": schema.StringAttribute{
+				Required: true,
+			},
+			"type": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString("couchbase"),
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"storage_backend": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString("couchstore"),
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"memory_allocation_in_mb": schema.Int64Attribute{
+				Optional: true,
+				Default:  int64default.StaticInt64(100),
+				Computed: true,
+			},
+			"bucket_conflict_resolution": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString("seqno"),
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"durability_level": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString("none"),
+				Computed: true,
+			},
+			"replicas": schema.Int64Attribute{
+				Optional: true,
+				Default:  int64default.StaticInt64(1),
+				Computed: true,
+			},
+			"flush": schema.BoolAttribute{
+				Optional: true,
+				Default:  booldefault.StaticBool(false),
+				Computed: true,
+			},
+			"time_to_live_in_seconds": schema.Int64Attribute{
+				Optional: true,
+				Default:  int64default.StaticInt64(0),
+				Computed: true,
+			},
+			"eviction_policy": schema.StringAttribute{
+				Optional: true,
+				Default:  stringdefault.StaticString("fullEviction"),
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
 			"stats": schema.SingleNestedAttribute{
 				Computed: true,
 				Attributes: map[string]schema.Attribute{
-					"item_count":         int64Attribute(computed),
-					"ops_per_second":     int64Attribute(computed),
-					"disk_used_in_mib":   int64Attribute(computed),
-					"memory_used_in_mib": int64Attribute(computed),
+					"item_count": schema.Int64Attribute{
+						Computed: true,
+					},
+					"ops_per_second": schema.Int64Attribute{
+						Computed: true,
+					},
+					"disk_used_in_mib": schema.Int64Attribute{
+						Computed: true,
+					},
+					"memory_used_in_mib": schema.Int64Attribute{
+						Computed: true,
+					},
 				},
 			},
 		},
