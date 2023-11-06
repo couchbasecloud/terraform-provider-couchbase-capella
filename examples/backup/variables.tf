c39911d4579478d0c867b15dc985eec11e50ef4a
Nidhi Kumar <nidhi.kumar@couchbase.com>
2023-11-06 13:26:47 -0800

    fixed variables.tf

--- examples/backup/variables.tf
+++ examples/backup/variables.tf
@@ -32,14 +32,14 @@ variable "backup" {
 }
 
 # ToDo Required for Backup Schedule, tracking under -https://couchbasecloud.atlassian.net/browse/AV-66698
-variable "weekly_schedule" {
-  description = ""
-
-  type = object({
-    day_of_week = optional(string)
-    start_at = optional(number)
-    incremental_every = optional(number)
-    retention_time = optional(string)
-    cost_optimized_retention = optional(bool)
-  })
-}
+#variable "weekly_schedule" {
+#  description = ""
+#
+#  type = object({
+#    day_of_week = optional(string)
+#    start_at = optional(number)
+#    incremental_every = optional(number)
+#    retention_time = optional(string)
+#    cost_optimized_retention = optional(bool)
+#  })
+#}
