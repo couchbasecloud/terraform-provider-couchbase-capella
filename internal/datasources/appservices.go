2bbb00e631f63892762ad6d62a2f143fa761b83a
nidhi07kumar <78451190+nidhi07kumar@users.noreply.github.com>
2023-11-08 13:11:34 -0800

    [AV-64820] App Services Terraform Provider Feature Branch (#84)
    Co-authored-by: Matty Maclean <matty.maclean@couchbase.com>
    Co-authored-by: Talina Shrotriya <5362897+Talina06@users.noreply.github.com>
    Co-authored-by: matty271828 <65253959+matty271828@users.noreply.github.com>
    Co-authored-by: Aniket Kumar <aniket.kumar@couchbase.com>
    Co-authored-by: aniket-Kumar-c <115682403+aniket-Kumar-c@users.noreply.github.com>
    Co-authored-by: Priya Rajagopal <rajagp@users.noreply.github.com>

---
+++ internal/datasources/appservices.go
@@ -0,0 +1,138 @@
+package datasources
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"terraform-provider-capella/internal/api/appservice"
+
+	"terraform-provider-capella/internal/api"
+	"terraform-provider-capella/internal/errors"
+	providerschema "terraform-provider-capella/internal/schema"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &AppServices{}
+	_ datasource.DataSourceWithConfigure = &AppServices{}
+)
+
+// AppServices is the AppServices data source implementation.
+type AppServices struct {
+	*providerschema.Data
+}
+
+// NewAppServices is a helper function to simplify the provider implementation.
+func NewAppServices() datasource.DataSource {
+	return &AppServices{}
+}
+
+// Metadata returns the app services data source type name.
+func (d *AppServices) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_app_services"
+}
+
+// Schema defines the schema for the AppServices data source.
+func (d *AppServices) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = AppServiceSchema()
+}
+
+// Read refreshes the Terraform state with the latest data of app services.
+func (d *AppServices) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state providerschema.AppServices
+	diags := req.Config.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if state.OrganizationId.IsNull() {
+		resp.Diagnostics.AddError(
+			"Error reading app services",
+			"Could not read app services, unexpected error: organization ID cannot be empty.",
+		)
+		return
+	}
+
+	var (
+		organizationId = state.OrganizationId.ValueString()
+	)
+
+	response, err := d.Client.Execute(
+		fmt.Sprintf("%s/v4/organizations/%s/appservices", d.HostURL, organizationId),
+		http.MethodGet,
+		nil,
+		d.Token,
+		nil,
+	)
+	switch err := err.(type) {
+	case nil:
+	case api.Error:
+		resp.Diagnostics.AddError(
+			"Error Reading Capella App Services",
+			fmt.Sprintf("Could not read app services in organization %s, unexpected error: %s", organizationId, err.CompleteError()),
+		)
+		return
+	default:
+		resp.Diagnostics.AddError(
+			"Error Reading Capella App Services",
+			fmt.Sprintf("Could not read app services in organization %s, unexpected error: %s", organizationId, err.Error()),
+		)
+		return
+	}
+
+	appServicesResp := appservice.GetAppServicesResponse{}
+	err = json.Unmarshal(response.Body, &appServicesResp)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error reading app services",
+			"Could not read app services, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	for _, appService := range appServicesResp.Data {
+		audit := providerschema.NewCouchbaseAuditData(appService.Audit)
+
+		auditObj, diags := types.ObjectValueFrom(ctx, audit.AttributeTypes(), audit)
+		if diags.HasError() {
+			resp.Diagnostics.AddError(
+				"Error Reading Capella App Services",
+				fmt.Sprintf("Could not read app services in organization %s, unexpected error: %s", organizationId, errors.ErrUnableToConvertAuditData),
+			)
+		}
+
+		newAppServiceData := providerschema.NewAppServiceData(&appService, organizationId, auditObj)
+		state.Data = append(state.Data, *newAppServiceData)
+	}
+
+	// Set state
+	diags = resp.State.Set(ctx, &state)
+
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Configure adds the provider configured client to the cluster data source.
+func (d *AppServices) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	data, ok := req.ProviderData.(*providerschema.Data)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *ProviderSourceData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+	d.Data = data
+}
