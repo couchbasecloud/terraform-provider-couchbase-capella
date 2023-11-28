2ecc6549869024776cfeaa047f473afdc925a22b
Nidhi Kumar <nidhi.kumar@couchbase.com>
2023-11-28 14:07:56 -0800

    allowlist create optional fields

--- examples/allowlist/create_allowlist.tf
+++ examples/allowlist/create_allowlist.tf
@@ -10,7 +10,7 @@ resource "capella_allowlist" "new_allowlist" {
   organization_id = var.organization_id
   project_id      = var.project_id
   cluster_id      = var.cluster_id
-  cidr            = var.cidr
-  comment         = var.comment
-  expires_at      = var.expires_at
+  cidr            = var.allowlist.cidr
+  comment         = var.allowlist.comment
+  expires_at      = var.allowlist.expires_at
 }
