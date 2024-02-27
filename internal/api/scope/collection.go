2a7c406f785ac794929500a198bd584a6fe36f46
PaulomeeCb <paulomee.de@couchbase.com>
2024-02-26 20:57:04 -0800

    "Adding resources/scope.go"

--- internal/api/scope/collection.go
+++ internal/api/scope/collection.go
@@ -3,7 +3,7 @@ package scope
 // Collection is the c sent by the Capella V4 Public API for any existing scope or collection.
 type Collection struct {
 	// MaxTTL Max TTL of the collection.
-	MaxTTL *int `json:"maxTTL,omitempty"`
+	MaxTTL *int64 `json:"maxTTL,omitempty"`
 
 	// Name is the Name of the collection.
 	Name *string `json:"name,omitempty"`
