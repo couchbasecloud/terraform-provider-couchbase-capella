5a48e32cb7b73946df650137c1c6f90e195274d2
PaulomeeCb <paulomee.de@couchbase.com>
2024-02-26 19:47:44 -0800

    Adding scope directory to internal/api

---
+++ internal/api/scope/collection.go
@@ -0,0 +1,13 @@
+package scope
+
+// Collection is the c sent by the Capella V4 Public API for any existing scope or collection.
+type Collection struct {
+	// MaxTTL Max TTL of the collection.
+	MaxTTL *int `json:"maxTTL,omitempty"`
+
+	// Name is the Name of the collection.
+	Name *string `json:"name,omitempty"`
+
+	// Uid is the UID of the collection.
+	Uid *string `json:"uid,omitempty"`
+}
