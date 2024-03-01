aaf8eec8f95381a0ae1437fa33d74328bdb21594
ajsqr <ajay.ajith@couchbase.com>
2024-03-01 17:34:50 +0530

    defined error

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -158,4 +158,7 @@ var (
 
 	// ErrIfMatchCannotBeSetWhileCreate is returned when if_match is set during create operation.
 	ErrIfMatchCannotBeSetWhileCreate = errors.New("if_match attribute cannot be set during create operation")
+
+	// ErrRatelimit is returned when an API key reaches the ratelimit
+	ErrRatelimit = errors.New("api key reached the ratelimit")
 )
