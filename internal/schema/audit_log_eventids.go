e58e1df2c6b73b417236536394dd24ee40be1344
Hitesh Walia <hitesh.walia@couchbase.com>
2024-03-04 20:15:11 -0800

    add schema file

---
+++ internal/schema/audit_log_eventids.go
@@ -0,0 +1,41 @@
+package schema
+
+import (
+	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/errors"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type AuditLogEventIDs struct {
+	// OrganizationId is the organizationId of the capella.
+	OrganizationId types.String `tfsdk:"organization_id"`
+
+	// ProjectId is the projectId of the capella tenant.
+	ProjectId types.String `tfsdk:"project_id"`
+
+	// ClusterId is the clusterId of the capella tenant.
+	ClusterId types.String `tfsdk:"cluster_id"`
+
+	Data []AuditLogEventID `tfsdk:"data"`
+}
+
+type AuditLogEventID struct {
+	Description types.String `tfsdk:"description"`
+	Id          types.Int64  `tfsdk:"id"`
+	Module      types.String `tfsdk:"module"`
+	Name        types.String `tfsdk:"name"`
+}
+
+// Validate is used to verify that all the fields in the datasource
+// have been populated.
+func (a *AuditLogEventIDs) Validate() error {
+	if a.OrganizationId.IsNull() {
+		return errors.ErrOrganizationIdMissing
+	}
+	if a.ProjectId.IsNull() {
+		return errors.ErrProjectIdMissing
+	}
+	if a.ClusterId.IsNull() {
+		return errors.ErrClusterIdMissing
+	}
+	return nil
+}
