050cd1bf24d5603068975594ff63fcbf79b79a90
Hitesh Walia <hitesh.walia@couchbase.com>
2024-03-05 10:55:41 -0800

    add example

---
+++ examples/auditlogseventids/variables.tf
@@ -0,0 +1,20 @@
+variable "organization_id" {
+  description = "Capella Organization ID"
+}
+
+variable "project_id" {
+  description = "Capella Project ID"
+}
+
+variable "cluster_id" {
+  description = "Capella Cluster ID"
+}
+
+variable "auth_token" {
+  description = "Authentication API Key"
+  sensitive   = true
+}
+
+variable "host" {
+  description = "The Host URL of Couchbase Cloud."
+}
