1db31c19f00280df73c4cc4c8e603c2bd34b66b0
Hitesh Walia <hitesh.walia@couchbase.com>
2024-03-12 22:27:09 -0700

    update example

--- examples/auditlogseventids/get_auditlogsettings.tf
+++
@@ -1,9 +0,0 @@
-output "existing_auditlogeventids" {
-  value = data.couchbase-capella_auditlogeventids.existing_auditlogeventids
-}
-
-data "couchbase-capella_auditlogeventids" "existing_auditlogeventids" {
-  organization_id = var.organization_id
-  project_id      = var.project_id
-  cluster_id      = var.cluster_id
-}
