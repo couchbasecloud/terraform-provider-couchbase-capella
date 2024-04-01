3b52032ed43e29c11edc04029c8eee21a2ab08b3
PaulomeeCb <paulomee.de@couchbase.com>
2024-04-01 15:30:49 -0700

    Updating unit tests for schedule on/off

--- internal/schema/cluster_onoff_schedule_test.go
+++ internal/schema/cluster_onoff_schedule_test.go
@@ -17,6 +17,7 @@ func TestNewClusterOnOffScheduleSchemaValidate(t *testing.T) {
 		expectedOrganizationId string
 		expectedProjectId      string
 		expectedClusterId      string
+		expectedTimeZone       string
 	}
 
 	tests := []test{
@@ -32,15 +33,17 @@ func TestNewClusterOnOffScheduleSchemaValidate(t *testing.T) {
 			expectedClusterId:      "300",
 		},
 		{
-			name: "[POSITIVE] project ID, organization ID, and cluster ID are passed via terraform apply",
+			name: "[POSITIVE] project ID, organization ID, and cluster ID, timezone, are passed via terraform apply",
 			input: ClusterOnOffSchedule{
 				ClusterId:      basetypes.NewStringValue("100"),
 				ProjectId:      basetypes.NewStringValue("200"),
 				OrganizationId: basetypes.NewStringValue("300"),
+				Timezone:       basetypes.NewStringValue("US/Pacific"),
 			},
 			expectedClusterId:      "100",
 			expectedProjectId:      "200",
 			expectedOrganizationId: "300",
+			expectedTimeZone:       "US/Pacific",
 		},
 		{
 			name: "[NEGATIVE] only timezone is passed via terraform apply",
