9a313bd07474410084d08236ff17daca9f038af9
Hitesh Walia <hitesh.walia@couchbase.com>
2024-04-29 10:50:22 -0700

    fix export schema

--- internal/resources/audit_log_export_schema.go
+++ internal/resources/audit_log_export_schema.go
@@ -7,16 +7,16 @@ import (
 func AuditLogExportSchema() schema.Schema {
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"id":                     stringAttribute([]string{required}),
+			"id":                     stringAttribute([]string{computed}),
 			"organization_id":        stringAttribute([]string{required}),
 			"project_id":             stringAttribute([]string{required}),
 			"cluster_id":             stringAttribute([]string{required}),
-			"audit_log_download_url": stringAttribute([]string{required}),
-			"expiration":             stringAttribute([]string{required}),
+			"audit_log_download_url": stringAttribute([]string{computed}),
+			"expiration":             stringAttribute([]string{computed}),
 			"start":                  stringAttribute([]string{required}),
 			"end":                    stringAttribute([]string{required}),
-			"created_at":             stringAttribute([]string{required}),
-			"status":                 stringAttribute([]string{required}),
+			"created_at":             stringAttribute([]string{computed}),
+			"status":                 stringAttribute([]string{computed}),
 		},
 	}
 }
