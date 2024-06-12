9a0348c5933b2a4a7275f8cbbb4c27cc61af1f24
PaulomeeCb <paulomee.de@couchbase.com>
2024-06-12 14:19:58 -0700

    files for list peering

---
+++ examples/network_peer/variables.tf
@@ -0,0 +1,47 @@
+variable "auth_token" {
+  description = "Authentication API Key"
+  sensitive   = true
+}
+
+variable "organization_id" {
+  description = "Capella Organization ID"
+}
+
+variable "project_id" {
+  description = "Capella Project ID"
+}
+
+variable "cluster_id" {
+  description = "Capella Cluster ID"
+}
+
+variable "network_peer" {
+  description = "Network Peer configuration details useful for creation"
+
+  type = object({
+    name               = string
+    provider_type      = string
+  })
+}
+
+variable "AWS_config" {
+  description = "AWS configuration details useful for network peer creation"
+
+  type = object({
+    account_id = optional(string)
+    vpc_id     = optional(string)
+    cidr       = string
+    region     = optional(string)
+  })
+}
+
+variable "GCP_config" {
+  description = "GCP configuration details useful for network peer creation"
+
+  type = object({
+    network_name    = optional(string)
+    project_id      = optional(string)
+    cidr            = string
+    service_account = optional(string)
+  })
+}
