4880b1fe13b5c804d23fd631bb239c5aa7867745
PaulomeeCb <paulomee.de@couchbase.com>
2024-07-02 15:17:47 -0700

    Updating variables.tf in getting started

--- examples/getting_started/network_peer.tf
+++ examples/getting_started/network_peer.tf
@@ -6,8 +6,8 @@ output "network_peer" {
 
 resource "couchbase-capella_network_peer" "new_network_peer" {
   organization_id = var.organization_id
-  project_id      = var.project_id
-  cluster_id      = var.cluster_id
+  project_id      = couchbase-capella_project.new_project.id
+  cluster_id      = couchbase-capella_cluster.new_cluster.id
   name            = var.network_peer.name
   provider_type   = var.network_peer.provider_type
   provider_config = {
