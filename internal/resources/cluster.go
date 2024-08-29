9f1951651d6138b0f79f184ba752c033f1b1b25d
Laura Silaja <Laurasila0@gmail.com>
2024-08-29 13:06:09 +0100

    AV-84160 make cluster timezone optional (#215)
    Co-authored-by: Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>

--- internal/resources/cluster.go
+++ internal/resources/cluster.go
@@ -85,9 +85,20 @@ func (c *Cluster) Create(ctx context.Context, req resource.CreateRequest, resp *
 			Type:   clusterapi.CloudProviderType(plan.CloudProvider.Type.ValueString()),
 		},
 		Support: clusterapi.Support{
-			Plan:     clusterapi.SupportPlan(plan.Support.Plan.ValueString()),
-			Timezone: clusterapi.SupportTimezone(plan.Support.Timezone.ValueString()),
+			Plan: clusterapi.SupportPlan(plan.Support.Plan.ValueString()),
 		},
+	}
+
+	if !plan.Support.Timezone.IsNull() && !plan.Support.Timezone.IsUnknown() {
+		clusterRequest.Support.Timezone = clusterapi.SupportTimezone(plan.Support.Timezone.ValueString())
+
+		if clusterRequest.Support.Plan == clusterapi.SupportPlan("basic") && clusterRequest.Support.Timezone != clusterapi.SupportTimezone("PT") {
+			resp.Diagnostics.AddError(
+				"Error creating cluster",
+				"Could not create cluster, unexpected error: Invalid timezone provided for basic cluster",
+			)
+			return
+		}
 	}
 
 	if !plan.Description.IsNull() && !plan.Description.IsUnknown() {
@@ -322,9 +333,20 @@ func (c *Cluster) Update(ctx context.Context, req resource.UpdateRequest, resp *
 		Description: plan.Description.ValueString(),
 		Name:        plan.Name.ValueString(),
 		Support: clusterapi.Support{
-			Plan:     clusterapi.SupportPlan(plan.Support.Plan.ValueString()),
-			Timezone: clusterapi.SupportTimezone(plan.Support.Timezone.ValueString()),
+			Plan: clusterapi.SupportPlan(plan.Support.Plan.ValueString()),
 		},
+	}
+
+	if !plan.Support.Timezone.IsNull() && !plan.Support.Timezone.IsUnknown() {
+		ClusterRequest.Support.Timezone = clusterapi.SupportTimezone(plan.Support.Timezone.ValueString())
+
+		if ClusterRequest.Support.Plan == clusterapi.SupportPlan("basic") && ClusterRequest.Support.Timezone != clusterapi.SupportTimezone("PT") {
+			resp.Diagnostics.AddError(
+				"Error creating cluster",
+				"Could not update cluster, unexpected error: Invalid timezone provided for basic cluster",
+			)
+			return
+		}
 	}
 
 	serviceGroups, err := c.morphToApiServiceGroups(plan)
