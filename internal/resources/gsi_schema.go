89e6ce78cb7539d0ff4fa6632f901b4374089c24
Hitesh Walia <hitesh.walia@couchbase.com>
2024-11-20 20:12:22 -0800

    fix panic on read

--- internal/resources/gsi_schema.go
+++ internal/resources/gsi_schema.go
@@ -2,14 +2,31 @@ package resources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/resources/custom_plan_modifiers"
 )
 
 func GsiSchema() schema.Schema {
+	defaultObject, _ := types.ObjectValue(
+		map[string]attr.Type{
+			"defer_build":   types.BoolType,
+			"num_replica":   types.Int64Type,
+			"num_partition": types.Int64Type,
+		},
+		map[string]attr.Value{
+			"defer_build":   types.BoolValue(false),
+			"num_replica":   types.Int64Value(0),
+			"num_partition": types.Int64Null(),
+		})
+
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"organization_id": stringAttribute([]string{required, requiresReplace}),
@@ -29,13 +46,19 @@ func GsiSchema() schema.Schema {
 			"partition_by": stringListAttribute(optional, requiresReplace),
 			"with": schema.SingleNestedAttribute{
 				Optional: true,
+				Computed: true,
+				Default:  objectdefault.StaticValue(defaultObject),
 				Attributes: map[string]schema.Attribute{
 					"defer_build": schema.BoolAttribute{
 						Optional:      true,
+						Computed:      true,
+						Default:       booldefault.StaticBool(false),
 						PlanModifiers: []planmodifier.Bool{custom_plan_modifiers.ImmutableBoolAttribute()},
 					},
 					"num_replica": schema.Int64Attribute{
 						Optional: true,
+						Computed: true,
+						Default:  int64default.StaticInt64(0),
 					},
 					"num_partition": schema.Int64Attribute{
 						Optional: true,
