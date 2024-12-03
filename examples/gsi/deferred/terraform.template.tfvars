7147d366f6fffee59b4772e16bf910e33dc9e680
Hitesh Walia <hitesh.walia@couchbase.com>
2024-12-03 11:50:20 -0800

    make tffmt

--- examples/gsi/deferred/terraform.template.tfvars
+++ examples/gsi/deferred/terraform.template.tfvars
@@ -4,7 +4,7 @@ organization_id = "<organization_id>"
 project_id      = "<project_id>"
 cluster_id      = "<cluster_id>"
 
-bucket_name = "api"
-scope_name = "metrics"
+bucket_name     = "api"
+scope_name      = "metrics"
 collection_name = "memory"
 
--- examples/gsi/non_deferred/create_index.tf
+++ examples/gsi/non_deferred/create_index.tf
@@ -1,10 +1,10 @@
 resource "couchbase-capella_query_indexes" "idx" {
   organization_id = var.organization_id
-  project_id = var.project_id
-  cluster_id = var.cluster_id
+  project_id      = var.project_id
+  cluster_id      = var.cluster_id
 
-  bucket_name = var.bucket_name
-  scope_name = var.scope_name
+  bucket_name     = var.bucket_name
+  scope_name      = var.scope_name
   collection_name = var.collection_name
 
   index_name = var.index_name
