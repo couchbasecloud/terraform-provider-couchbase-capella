36952c2585bc804bd5b42a7f1f4ad522ae53e1a6
Hitesh Walia <hitesh.walia@couchbase.com>
2024-12-18 15:01:42 -0800

    destroy project

--- internal/resources/acceptance_tests/project.go
+++ internal/resources/acceptance_tests/project.go
@@ -9,7 +9,7 @@ import (
 	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api"
 )
 
-func CreateProject(ctx context.Context, client *api.Client) error {
+func createProject(ctx context.Context, client *api.Client) error {
 	projectRequest := api.CreateProjectRequest{
 		Name: "tf_acc_test_project_common",
 	}
@@ -33,6 +33,23 @@ func CreateProject(ctx context.Context, client *api.Client) error {
 	}
 
 	ProjectId = projectResponse.Id.String()
+
+	return nil
+}
+
+func destroyProject(ctx context.Context, client *api.Client) error {
+	url := fmt.Sprintf("%s/v4/organizations/%s/projects/%s", Host, OrgId, ProjectId)
+	cfg := api.EndpointCfg{Url: url, Method: http.MethodDelete, SuccessStatus: http.StatusNoContent}
+	_, err := client.ExecuteWithRetry(
+		context.Background(),
+		cfg,
+		nil,
+		Token,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
