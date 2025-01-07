fdfaffb2f173f8ec1ccc0059872836e46bdcf527
Hitesh Walia <hitesh.walia@couchbase.com>
2025-01-06 19:13:42 -0800

    fix org acc test

--- internal/resources/acceptance_tests/organization_acceptance_test.go
+++ internal/resources/acceptance_tests/organization_acceptance_test.go
@@ -7,18 +7,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccOrganizationDataSource(t *testing.T) {
+func TestAccReadOrganization(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Create and Read testing
 			{
 				Config: testAccOrganizationResourceConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.couchbase-capella_organization.get_organization", "name"),
 					resource.TestCheckResourceAttr("data.couchbase-capella_organization.get_organization", "organization_id", OrgId),
 					resource.TestCheckResourceAttrSet("data.couchbase-capella_organization.get_organization", "audit.created_at"),
-					resource.TestCheckResourceAttrSet("data.couchbase-capella_organization.get_organization", "audit.modified_by"),
 					resource.TestCheckResourceAttrSet("data.couchbase-capella_organization.get_organization", "audit.modified_at"),
 					resource.TestCheckResourceAttrSet("data.couchbase-capella_organization.get_organization", "audit.version"),
 				),
