5b8b2f05e43675ecc21a348dbefdd0f4e7bf2414
Hitesh Walia <hitesh.walia@couchbase.com>
2025-01-06 19:31:48 -0800

    fix sample bucket acc test

--- internal/resources/acceptance_tests/sample_bucket_acceptance_test.go
+++ internal/resources/acceptance_tests/sample_bucket_acceptance_test.go
@@ -2,43 +2,23 @@ package acceptance_tests
 
 import (
 	"fmt"
+	acctest "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/testing"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"regexp"
 	"testing"
-	"time"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-
-	acctest "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/testing"
 )
 
-func TestAccSampleBucketTestCases(t *testing.T) {
+func TestAccSampleBucket(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccSampleBucketWithTravelSampleConfig(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					acctest.TestAccWait(time.Second*10),
 					resource.TestCheckResourceAttr("couchbase-capella_sample_bucket.add_sample_bucket_travel", "name", "travel-sample"),
 					resource.TestCheckResourceAttrSet("couchbase-capella_sample_bucket.add_sample_bucket_travel", "id"),
 				),
-				ExpectNonEmptyPlan: true,
 			},
-			{
-				Config: testAccSampleBucketWithAllBucketsConfig(),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("couchbase-capella_sample_bucket.add_sample_bucket_travel", "name", "travel-sample"),
-					resource.TestCheckResourceAttrSet("couchbase-capella_sample_bucket.add_sample_bucket_travel", "id"),
-
-					resource.TestCheckResourceAttr("couchbase-capella_sample_bucket.add_sample_bucket_gamesim", "name", "gamesim-sample"),
-					resource.TestCheckResourceAttrSet("couchbase-capella_sample_bucket.add_sample_bucket_gamesim", "id"),
-
-					resource.TestCheckResourceAttr("couchbase-capella_sample_bucket.add_sample_bucket_beer", "name", "beer-sample"),
-					resource.TestCheckResourceAttrSet("couchbase-capella_sample_bucket.add_sample_bucket_beer", "id"),
-				),
-				ExpectNonEmptyPlan: true,
-			},
-
 			//Invalid Sample Data input
 			{
 				Config:      testAccWithInvalidSampleInputConfig(),
@@ -46,6 +26,18 @@ func TestAccSampleBucketTestCases(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccSampleBucketWithTravelSampleConfig() string {
+	return fmt.Sprintf(`
+%[1]s
+resource "couchbase-capella_sample_bucket" "add_sample_bucket_travel" {
+  organization_id = "%[2]s"
+  project_id      = "%[3]s"
+  cluster_id      = "%[4]s"
+  name			  = "travel-sample"
+}
+`, ProviderBlock, OrgId, ProjectId, ClusterId)
 }
 
 func testAccWithInvalidSampleInputConfig() string {
@@ -61,55 +53,4 @@ resource "couchbase-capella_sample_bucket" "add_sample_bucket_invalid" {
   name			  = "invalid-sample"
 }
 `, ProviderBlock, OrgId, ProjectId, ClusterId)
-}
-
-func testAccSampleBucketWithTravelSampleConfig() string {
-	return fmt.Sprintf(`
-%[1]s
-output "sample_bucket_travel"{
-  value = couchbase-capella_sample_bucket.add_sample_bucket_travel
-}
-resource "couchbase-capella_sample_bucket" "add_sample_bucket_travel" {
-  organization_id = "%[2]s"
-  project_id      = "%[3]s"
-  cluster_id      = "%[4]s"
-  name			  = "travel-sample"
-}
-`, ProviderBlock, OrgId, ProjectId, ClusterId)
-}
-
-func testAccSampleBucketWithAllBucketsConfig() string {
-	return fmt.Sprintf(`
-%[1]s
-
-output "sample_bucket_beer"{
-  value = couchbase-capella_sample_bucket.add_sample_bucket_beer
-}
-output "sample_bucket_gamesim"{
-  value = couchbase-capella_sample_bucket.add_sample_bucket_gamesim
-}
-output "sample_bucket_travel"{
-  value = couchbase-capella_sample_bucket.add_sample_bucket_travel
-}
-resource "couchbase-capella_sample_bucket" "add_sample_bucket_travel" {
-  organization_id = "%[2]s"
-  project_id      = "%[3]s"
-  cluster_id      = "%[4]s"
-  name			  = "travel-sample"
-}
-resource "couchbase-capella_sample_bucket" "add_sample_bucket_beer" {
-  organization_id = "%[2]s"
-  project_id      = "%[3]s"
-  cluster_id      = "%[4]s"
-  name			  = "beer-sample"
-}
-resource "couchbase-capella_sample_bucket" "add_sample_bucket_gamesim" {
-  organization_id = "%[2]s"
-  project_id      = "%[3]s"
-  cluster_id      = "%[4]s"
-  name			  = "gamesim-sample"
-}
-
-`, ProviderBlock, OrgId, ProjectId, ClusterId)
-
 }
