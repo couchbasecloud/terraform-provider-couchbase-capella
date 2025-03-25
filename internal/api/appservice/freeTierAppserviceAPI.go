1c7ca307f78fdedefde9dc161ffe9bbf576c69d9
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-03-25 20:46:46 +0530

    adding free tier app service resource

---
+++ internal/api/appservice/freeTierAppserviceAPI.go
@@ -0,0 +1,28 @@
+package appservice
+
+// CreateFreeTierAppServiceRequest is the request payload sent to the Capella V4 Public API in order to create a new free-tier app service.
+// An App Service synchronizes data between the Couchbase Capella database and any mobile applications.
+// App Service is a fully managed application backend designed to provide data synchronization for mobile/IoT applications and the Capella Cloud Service.
+//
+// To learn more about App Services, see https://docs.couchbase.com/cloud/app-services/index.html
+//
+// In order to access this endpoint, the provided API key must have at least one of the roles referenced below:
+//
+// Organization Owner
+// Project Owner
+// Project Manager
+// To learn more, see https://docs.couchbase.com/cloud/organizations/organization-projects-overview.html
+
+type CreateFreeTierAppServiceRequest struct {
+	// Description is the description for the free-tier app service (up to 256 characters).
+	Description *string `json:"description,omitempty"`
+	// Name of the free-tier app service.
+	Name string `json:"name"`
+}
+
+type UpdateFreeTierAppServiceRequest struct {
+	// Description is the description for the free-tier app service (up to 256 characters).
+	Description *string `json:"description,omitempty"`
+	// Name of the free-tier app service.
+	Name string `json:"name"`
+}
---
+++ internal/resources/free_tier_appservice_schema.go
@@ -0,0 +1,51 @@
+package resources
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+func FreeTierAppServiceSchema() schema.Schema {
+	return schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"organization_id": stringAttribute([]string{required, requiresReplace}, validator.String(
+				stringvalidator.LengthAtLeast(1),
+			)),
+			"project_id": stringAttribute([]string{required, requiresReplace}, validator.String(
+				stringvalidator.LengthAtLeast(1),
+			)),
+			"cluster_id": stringAttribute([]string{required, requiresReplace}, validator.String(
+				stringvalidator.LengthAtLeast(1),
+			)),
+			"name":           stringAttribute([]string{required}),
+			"description":    stringDefaultAttribute("", optional, computed),
+			"nodes":          int64Attribute(computed, useStateForUnknown),
+			"cloud_provider": stringAttribute([]string{computed, useStateForUnknown}),
+			"current_state":  stringAttribute([]string{computed}),
+			"version":        stringAttribute([]string{computed}),
+			"compute": schema.SingleNestedAttribute{
+				Computed: true,
+				Attributes: map[string]schema.Attribute{
+					"cpu": int64Attribute(computed, useStateForUnknown),
+					"ram": int64Attribute(computed, useStateForUnknown),
+				},
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"audit": computedAuditAttribute(),
+			"plan":  stringAttribute([]string{computed}),
+			"etag":  stringAttribute([]string{computed}),
+		},
+	}
+}
