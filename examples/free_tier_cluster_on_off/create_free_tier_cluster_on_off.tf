59f46180907230c3135c61396bc9823d31ca251c
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-04-15 13:19:05 +0530

    addressing comments

--- examples/free_tier_cluster_on_off/create_free_tier_cluster_on_off.tf
+++ examples/free_tier_cluster_on_off/create_free_tier_cluster_on_off.tf
@@ -3,10 +3,10 @@ output "new_free_tier_cluster_on_off" {
 }
 
 resource "couchbase-capella_free_tier_cluster_on_off" "new_free_tier_cluster_on_off" {
-  organization_id            = var.organization_id
-  project_id                 = var.project_id
-  cluster_id                 = var.cluster_id
-  state                      = var.state
+  organization_id = var.organization_id
+  project_id      = var.project_id
+  cluster_id      = var.cluster_id
+  state           = var.state
 
 }
 
