41824e8c32d3c1744959dbf59b580c9a42466578
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-02 13:36:14 -0700

    Example files

---
+++ examples/data-sources/couchbase-capella_private_endpoint_service/data-source.tf
@@ -0,0 +1,5 @@
+data "couchbase-capella_private_endpoint_service" "service_status" {
+  organization_id = "<organization_id>"
+  project_id      = "<project_id>"
+  cluster_id      = "<cluster_id>"
+}
---
+++ examples/resources/couchbase-capella_private_endpoint_service/import.sh
@@ -0,0 +1,2 @@
+terraform import couchbase-capella_private_endpoint_service.new_service \
+cluster_id=ffffffff-aaaa-1414-eeee-000000000000,project_id=ffffffff-aaaa-1414-eeee-000000000000,organization_id=ffffffff-aaaa-1414-eeee-000000000000
