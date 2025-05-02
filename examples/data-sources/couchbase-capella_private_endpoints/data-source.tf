320d33b98ca5def4484462390017bbe529de32ab
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-02 13:25:18 -0700

    Formatting

--- examples/data-sources/couchbase-capella_private_endpoints/data-source.tf
+++ examples/data-sources/couchbase-capella_private_endpoints/data-source.tf
@@ -1,5 +1,5 @@
 data "couchbase-capella_private_endpoints" "list_endpoints" {
   organization_id = "<organization_id>"
-    project_id      = "<project_id>"
-    cluster_id      = "<cluster_id>"
+  project_id      = "<project_id>"
+  cluster_id      = "<cluster_id>"
 }
--- examples/resources/couchbase-capella_private_endpoints/resource.tf
+++ examples/resources/couchbase-capella_private_endpoints/resource.tf
@@ -1,6 +1,6 @@
 resource "couchbase-capella_private_endpoints" "accept_endpoint" {
-     organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
-     project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
-     cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
-     endpoint_id     = "vpce-7"
-    }
+  organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
+  project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+  cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+  endpoint_id     = "vpce-7"
+}
