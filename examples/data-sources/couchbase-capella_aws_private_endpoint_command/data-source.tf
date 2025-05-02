f32d693c08e589103143c12d548a9cdf606dd640
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-02 14:16:18 -0700

    Formatting

--- examples/data-sources/couchbase-capella_aws_private_endpoint_command/data-source.tf
+++ examples/data-sources/couchbase-capella_aws_private_endpoint_command/data-source.tf
@@ -3,5 +3,5 @@ data "couchbase-capella_aws_private_endpoint_command" "aws_command" {
   project_id      = "<project_id>"
   cluster_id      = "<cluster_id>"
   vpc_id          = "vpc-1234"
-  subnet_ids      = "["subnet-1234",]"
+  subnet_ids      = ["subnet-1234"]
 }
