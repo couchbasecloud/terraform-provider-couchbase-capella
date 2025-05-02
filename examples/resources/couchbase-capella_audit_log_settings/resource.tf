d370a4fed7160ea352db1dcea7173eab574aac05
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-02 10:58:43 -0700

    Formatting

--- examples/resources/couchbase-capella_audit_log_settings/resource.tf
+++ examples/resources/couchbase-capella_audit_log_settings/resource.tf
@@ -1,8 +1,8 @@
 resource "couchbase-capella_audit_log_settings" "new_auditlogsettings" {
-  organization_id = "<organization_id>"
-  project_id      = "<project_id>"
-  cluster_id      = "<cluster_id>"
+  organization_id   = "<organization_id>"
+  project_id        = "<project_id>"
+  cluster_id        = "<cluster_id>"
   audit_enabled     = true
-  enabled_event_ids = [20488, 20490,20491,]
+  enabled_event_ids = [20488, 20490, 20491, ]
   disabled_users    = []
 }
