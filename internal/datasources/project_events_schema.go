1e977381381bfd9947b84589badf41ab444b1f8a
karanjain-ops <karan.jain@couchbase.com>
2025-05-05 17:07:16 +0530

    [AV-100867] feat: Added docs for project events.

--- internal/datasources/project_events_schema.go
+++ internal/datasources/project_events_schema.go
@@ -2,26 +2,67 @@ package datasources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // ProjectEventsSchema returns the schema for the ProjectEvents data source.
 func ProjectEventsSchema() schema.Schema {
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"organization_id": requiredStringAttribute,
-			"project_id":      optionalStringAttribute,
-			"cluster_ids":     optionalStringSetAttribute,
-			"user_ids":        optionalStringSetAttribute,
-			"severity_levels": optionalStringSetAttribute,
-			"tags":            optionalStringSetAttribute,
-			"from":            optionalStringAttribute,
-			"to":              optionalStringAttribute,
-			"page":            optionalInt64Attribute,
-			"per_page":        optionalInt64Attribute,
-			"sort_by":         optionalStringAttribute,
-			"sort_direction":  optionalStringAttribute,
-			"data":            computedEventAttributes,
-			"cursor":          computedCursorAttribute,
+			"organization_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The GUID4 ID of the Capella organization.",
+			},
+			"project_id": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "The GUID4 ID of the project.",
+			},
+			"cluster_ids": schema.SetAttribute{
+				ElementType:         types.StringType,
+				Optional:            true,
+				MarkdownDescription: "List of clusterIds to filter on. By default events corresponding to all clusters are returned.",
+			},
+			"user_ids": schema.SetAttribute{
+				ElementType:         types.StringType,
+				Optional:            true,
+				MarkdownDescription: "Filter by user UUID. Default is to return events corresponding to all users.",
+			},
+			"severity_levels": schema.SetAttribute{
+				ElementType:         types.StringType,
+				Optional:            true,
+				MarkdownDescription: "Filter by severity levels. Default is to return events corresponding to all supported severity levels.",
+			},
+			"tags": schema.SetAttribute{
+				ElementType:         types.StringType,
+				Optional:            true,
+				MarkdownDescription: "Filter by tags. Default is to return events corresponding to all supported tag. Tags are availability, billing, maintenance, performance, security, alert.",
+			},
+			"from": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "Start date in RFC3339 format. If not provided, events starting from last 24 hours are returned.",
+			},
+			"to": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "End datetime in the last 24 hours, RFC3339 format. Defaults to Now.",
+			},
+			"page": schema.Int64Attribute{
+				Optional:            true,
+				MarkdownDescription: "Sets the page you would like to view.",
+			},
+			"per_page": schema.Int64Attribute{
+				Optional:            true,
+				MarkdownDescription: "Sets the number of results you would like to have on each page.",
+			},
+			"sort_by": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "Sets the order of how you would like to sort the results and the key you would like to order by. Valid fields to sort the results are: severity, timestamp.",
+			},
+			"sort_direction": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "The order in which the items will be sorted.",
+			},
+			"data":   computedEventAttributes,
+			"cursor": computedCursorAttribute,
 		},
 	}
 }
