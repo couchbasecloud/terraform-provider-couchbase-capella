78bb5e1a9a251d652f0935cb0f7789b87406f095
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-06 22:37:44 +0530

    adding free tier on/off docs

---
+++ examples/resources/couchbase-capella_free_tier_cluster_on_off/resource.tf
@@ -0,0 +1,6 @@
+resource "couchbase-capella_free_tier_cluster_on_off" "new_free_tier_cluster_on_off" {
+  organization_id = "organization_id"
+  project_id      = "project_id"
+  cluster_id      = "cluster_id"
+  state           = "state"
+}
--- internal/resources/free_tier_cluster_on_off_schema.go
+++ internal/resources/free_tier_cluster_on_off_schema.go
@@ -3,16 +3,49 @@ package resources
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func FreeTierClusterOnOffSchema() schema.Schema {
 	return schema.Schema{
+		MarkdownDescription: "Manages the on/off state resource of a Free Tier Cluster.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))),
-			"project_id":      stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))),
-			"cluster_id":      stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))),
-			"state":           stringAttribute([]string{required}),
+			"organization_id": schema.StringAttribute{
+				MarkdownDescription: "The GUID4 ID of the organization.",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"project_id": schema.StringAttribute{
+				MarkdownDescription: "The GUID4 ID of the project.",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"cluster_id": schema.StringAttribute{
+				MarkdownDescription: "The GUID4 ID of the cluster.",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"state": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The on/off state of the Free Tier Cluster.",
+			},
 		},
 	}
 }
