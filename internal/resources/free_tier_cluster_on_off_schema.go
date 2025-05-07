4f6686f654c5b692f03599a79855c1d3136234da
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-07 14:21:49 +0530

    adding with description function

--- internal/resources/free_tier_cluster_on_off_schema.go
+++ internal/resources/free_tier_cluster_on_off_schema.go
@@ -3,49 +3,16 @@ package resources
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func FreeTierClusterOnOffSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "Manages the on/off state resource of a Free Tier Cluster.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": schema.StringAttribute{
-				MarkdownDescription: "The GUID4 ID of the organization.",
-				Required:            true,
-				Validators: []validator.String{
-					stringvalidator.LengthAtLeast(1),
-				},
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"project_id": schema.StringAttribute{
-				MarkdownDescription: "The GUID4 ID of the project.",
-				Required:            true,
-				Validators: []validator.String{
-					stringvalidator.LengthAtLeast(1),
-				},
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"cluster_id": schema.StringAttribute{
-				MarkdownDescription: "The GUID4 ID of the cluster.",
-				Required:            true,
-				Validators: []validator.String{
-					stringvalidator.LengthAtLeast(1),
-				},
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"state": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The on/off state of the Free Tier Cluster.",
-			},
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the organization."),
+			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the project."),
+			"cluster_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the cluster."),
+			"state":           WithDescription(stringAttribute([]string{required}), "The on/off state of the Free Tier Cluster."),
 		},
 	}
 }
