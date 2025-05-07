fb98c445910df39e1df5d4a6d9d4fe743cbfe85b
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-07 17:23:50 +0530

    adding data source docs

--- internal/datasources/backup_schema.go
+++ internal/datasources/backup_schema.go
@@ -4,49 +4,137 @@ import "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 
 func BackupSchema() schema.Schema {
 	return schema.Schema{
+		MarkdownDescription: "Manages backup datasource, retrieves backups associated with a bucket for a Couchbase Capella cluster.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": requiredStringAttribute,
-			"project_id":      requiredStringAttribute,
-			"cluster_id":      requiredStringAttribute,
-			"bucket_id":       requiredStringAttribute,
+			"organization_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The GUID4 ID of the organization.",
+			},
+			"project_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The GUID4 ID of the project.",
+			},
+			"cluster_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The GUID4 ID of the cluster.",
+			},
+			"bucket_id": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The ID of the bucket. It is the URL-compatible base64 encoding of the bucket name.",
+			},
 			"data": schema.ListNestedAttribute{
 				Computed: true,
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
-						"id":              computedStringAttribute,
-						"organization_id": computedStringAttribute,
-						"project_id":      computedStringAttribute,
-						"cluster_id":      computedStringAttribute,
-						"bucket_id":       computedStringAttribute,
-						"cycle_id":        computedStringAttribute,
-						"date":            computedStringAttribute,
-						"restore_before":  computedStringAttribute,
-						"status":          computedStringAttribute,
-						"method":          computedStringAttribute,
-						"bucket_name":     computedStringAttribute,
-						"source":          computedStringAttribute,
-						"cloud_provider":  computedStringAttribute,
+						"id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The GUID4 ID of the backup resource.",
+						},
+						"organization_id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The GUID4 ID of the organization.",
+						},
+						"project_id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The GUID4 ID of the project.",
+						},
+						"cluster_id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The GUID4 ID of the cluster.",
+						},
+						"bucket_id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The ID of the bucket. It is the URL-compatible base64 encoding of the bucket name.",
+						},
+						"cycle_id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The GUID4 ID of the cycle this backup belongs to.",
+						},
+						"date": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The RFC3339 timestamp representing the time at which backup was created.",
+						},
+						"restore_before": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The RFC3339 timestamp representing the time at which backup will expire",
+						},
+						"status": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The status of the backup. values being pending, ready and failed",
+						},
+						"method": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The mechanism of the backup. It can be either incremental or full.",
+						},
+						"bucket_name": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The name of the bucket for which the backup belongs to.",
+						},
+						"source": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The way a backup job was initiated. It can be either manual or scheduled.",
+						},
+						"cloud_provider": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The cloud provider where the cluster is hosted.",
+						},
 						"backup_stats": schema.SingleNestedAttribute{
 							Computed: true,
 							Attributes: map[string]schema.Attribute{
-								"size_in_mb": computedFloat64Attribute,
-								"items":      computedInt64Attribute,
-								"mutations":  computedInt64Attribute,
-								"tombstones": computedInt64Attribute,
-								"gsi":        computedInt64Attribute,
-								"fts":        computedInt64Attribute,
-								"cbas":       computedInt64Attribute,
-								"event":      computedInt64Attribute,
+								"size_in_mb": schema.Float64Attribute{
+									Computed:            true,
+									MarkdownDescription: "Size in MB is the total size of the backup in megabytes. It represents the amount of data that was backed up during the backup operation.",
+								},
+								"items": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of items saved during the backup.",
+								},
+								"mutations": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of mutations saved during the backup.",
+								},
+								"tombstones": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of tombstones saved during the backup.",
+								},
+								"gsi": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of global secondary indexes saved during the backup.",
+								},
+								"fts": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of full text search entities saved during the backup.",
+								},
+								"cbas": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of analytics entities saved during the backup.",
+								},
+								"event": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "The number of eventing entities saved during the backup.",
+								},
 							},
 						},
 						"elapsed_time_in_seconds": computedInt64Attribute,
 						"schedule_info": schema.SingleNestedAttribute{
 							Computed: true,
 							Attributes: map[string]schema.Attribute{
-								"backup_type": computedStringAttribute,
-								"backup_time": computedStringAttribute,
-								"increment":   computedInt64Attribute,
-								"retention":   computedStringAttribute,
+								"backup_type": schema.StringAttribute{
+									Computed:            true,
+									MarkdownDescription: "Represents whether the backup is a Weekly or Daily backup.",
+								},
+								"backup_time": schema.StringAttribute{
+									Computed:            true,
+									MarkdownDescription: "The timestamp indicating the backup created time.",
+								},
+								"increment": schema.Int64Attribute{
+									Computed:            true,
+									MarkdownDescription: "Represents interval in hours for incremental backup.",
+								},
+								"retention": schema.StringAttribute{
+									Computed:            true,
+									MarkdownDescription: "Represents retention time in days.",
+								},
 							},
 						},
 					},
--- internal/resources/backup_schema.go
+++ internal/resources/backup_schema.go
@@ -2,165 +2,53 @@ package resources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
 func BackupSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "Manages backup resource associated with a bucket for a Couchbase Capella cluster.",
 		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The GUID4 ID of the backup resource.",
-			},
-			"organization_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The GUID4 ID of the organization.",
-			},
-			"project_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The GUID4 ID of the project.",
-			},
-			"cluster_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The GUID4 ID of the cluster.",
-			},
-			"bucket_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The GUID4 ID of the bucket.",
-			},
-			"cycle_id": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The GUID4 ID of the cycle this backup belongs to.",
-			},
-			"date": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The RFC3339 timestamp representing the time at which backup was created.",
-			},
-			"restore_before": schema.StringAttribute{
-				Computed: true,
-				Optional: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The RFC3339 timestamp representing the time at which backup will expire.",
-			},
-			"status": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The status of the backup. values being pending, ready and failed",
-			},
-			"method": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The mechanism of the backup. It can be either incremental or full.",
-			},
-			"bucket_name": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The name of the bucket for which the backup belongs to.",
-			},
-			"source": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The way a backup job was initiated. It can be either manual or scheduled.",
-			},
-			"cloud_provider": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The cloud provider where the cluster is hosted.",
-			},
+			"id":              WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The GUID4 ID of the backup resource."),
+			"organization_id": WithDescription(stringAttribute([]string{required}), "The GUID4 ID of the organization."),
+			"project_id":      WithDescription(stringAttribute([]string{required}), "The GUID4 ID of the project."),
+			"cluster_id":      WithDescription(stringAttribute([]string{required}), "The GUID4 ID of the cluster."),
+			"bucket_id":       WithDescription(stringAttribute([]string{required}), "The ID of the bucket. It is the URL-compatible base64 encoding of the bucket name."),
+			"cycle_id":        WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The GUID4 ID of the cycle this backup belongs to."),
+			"date":            WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The RFC3339 timestamp representing the time at which backup was created."),
+			"restore_before":  WithDescription(stringAttribute([]string{computed, optional, useStateForUnknown}), "The RFC3339 timestamp representing the time at which backup will expire."),
+			"status":          WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The status of the backup. values being pending, ready and failed"),
+			"method":          WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The mechanism of the backup. It can be either incremental or full."),
+			"bucket_name":     WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The name of the bucket for which the backup belongs to."),
+			"source":          WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The source of the backup. It can be either cluster or bucket."),
+			"cloud_provider":  WithDescription(stringAttribute([]string{computed, useStateForUnknown}), "The cloud provider where the cluster is hosted."),
 			"backup_stats": schema.SingleNestedAttribute{
 				Computed:            true,
 				MarkdownDescription: "Represents various backup level data that couchbase provides.",
 				Attributes: map[string]schema.Attribute{
-					"size_in_mb": schema.Float64Attribute{
-						Computed:            true,
-						MarkdownDescription: "Size in MB is the total size of the backup in megabytes. It represents the amount of data that was backed up during the backup operation.",
-					},
-
-					"items": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of items saved during the backup.",
-					},
-					"mutations": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of mutations saved during the backup.",
-					},
-					"tombstones": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of tombstones saved during the backup.",
-					},
-					"gsi": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of global secondary indexes saved during the backup.",
-					},
-					"fts": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of full text search entities saved during the backup.",
-					},
-					"cbas": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of analytics entities saved during the backup.",
-					},
-					"event": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "The number of event entities saved during the backup.",
-					},
+					"size_in_mb": WithDescription(float64Attribute(computed), "Size in MB is the total size of the backup in megabytes. It represents the amount of data that was backed up during the backup operation."),
+					"items":      WithDescription(int64Attribute(computed), "The number of items saved during the backup."),
+					"mutations":  WithDescription(int64Attribute(computed), "The number of mutations saved during the backup."),
+					"tombstones": WithDescription(int64Attribute(computed), "The number of tombstones saved during the backup."),
+					"gsi":        WithDescription(int64Attribute(computed), "The number of global secondary indexes saved during the backup."),
+					"fts":        WithDescription(int64Attribute(computed), "The number of full text search entities saved during the backup."),
+					"cbas":       WithDescription(int64Attribute(computed), "The number of analytics entities saved during the backup."),
+					"event":      WithDescription(int64Attribute(computed), "The number of event entities saved during the backup."),
 				},
 				PlanModifiers: []planmodifier.Object{
 					objectplanmodifier.UseStateForUnknown(),
 				},
 			},
-			"elapsed_time_in_seconds": schema.Int64Attribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.Int64{
-					int64planmodifier.UseStateForUnknown(),
-				},
-				MarkdownDescription: "The amount of seconds that have elapsed between the creation and completion of the backup.",
-			},
+			"elapsed_time_in_seconds": WithDescription(int64Attribute(computed, useStateForUnknown), "The amount of seconds that have elapsed between the creation and completion of the backup."),
 			"schedule_info": schema.SingleNestedAttribute{
 				Computed:            true,
 				MarkdownDescription: "Represents the schedule information of the backup.",
 				Attributes: map[string]schema.Attribute{
-					"backup_type": schema.StringAttribute{
-						Computed:            true,
-						MarkdownDescription: "Represents whether the backup is a Weekly or Daily backup.",
-					},
-					"backup_time": schema.StringAttribute{
-						Computed:            true,
-						MarkdownDescription: "The timestamp indicating the backup created time.",
-					},
-					"increment": schema.Int64Attribute{
-						Computed:            true,
-						MarkdownDescription: "Represents interval in hours for incremental backup.",
-					},
-					"retention": schema.StringAttribute{
-						Computed:            true,
-						MarkdownDescription: "Represents retention time in days.",
-					},
+					"backup_type": WithDescription(stringAttribute([]string{computed}), "Represents whether the backup is a Weekly or Daily backup."),
+					"backup_time": WithDescription(stringAttribute([]string{computed}), "Represents the time at which the backup is scheduled to be taken."),
+					"increment":   WithDescription(int64Attribute(computed), "Represents interval in hours for incremental backup."),
+					"retention":   WithDescription(int64Attribute(computed), "Represents retention time in days."),
 				},
 				PlanModifiers: []planmodifier.Object{
 					objectplanmodifier.UseStateForUnknown(),
@@ -170,64 +58,22 @@ func BackupSchema() schema.Schema {
 				Optional:            true,
 				MarkdownDescription: "Represents restore configuration for the backup.",
 				Attributes: map[string]schema.Attribute{
-					"target_cluster_id": schema.StringAttribute{
-						Required:            true,
-						MarkdownDescription: "The ID of the target cluster to restore to.",
-					},
-					"source_cluster_id": schema.StringAttribute{
-						Required:            true,
-						MarkdownDescription: "The ID of the source cluster the restore is based on.",
-					},
-					"services": schema.StringAttribute{
-						Required:            true,
-						MarkdownDescription: "Services is a list of services to be restored. It is used to specify the services that should be included in the restore operation.",
-					},
-					"force_updates": schema.BoolAttribute{
-						Optional:            true,
-						MarkdownDescription: "Forces data in the Couchbase cluster to be overwritten even if the data in the cluster is newer.",
-					},
-					"auto_remove_collections": schema.BoolAttribute{
-						Optional:            true,
-						MarkdownDescription: "Automatically delete scopes/collections which are known to be deleted in the backup.",
-					},
-					"filter_keys": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Only restore data where the key matches a particular regular expression.",
-					},
-					"filter_values": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Only restore data where the value matches a particular regular expression.",
-					},
-					"include_data": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Restores only the data specified here.",
-					},
-					"exclude_data": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Skips restoring the data specified here.",
-					},
-					"map_data": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Specified when you want to restore source data into a different location.",
-					},
-					"replace_ttl": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "",
-					},
-					"replace_ttl_with": schema.StringAttribute{
-						Optional:            true,
-						MarkdownDescription: "Sets a new expiration (time-to-live) value for the specified keys. Values being none, all or expired",
-					},
-					"status": schema.StringAttribute{
-						Computed:            true,
-						MarkdownDescription: "Updates the expiration for the keys.",
-					},
+					"target_cluster_id":       WithDescription(stringAttribute([]string{required}), "The ID of the target cluster to restore to."),
+					"source_cluster_id":       WithDescription(stringAttribute([]string{required}), "The ID of the source cluster the restore is based on."),
+					"services":                WithDescription(stringListAttribute(required), "Services is a list of services to be restored. It is used to specify the services that should be included in the restore operation."),
+					"force_updates":           WithDescription(boolAttribute(optional), "Forces data in the Couchbase cluster to be overwritten even if the data in the cluster is newer."),
+					"auto_remove_collections": WithDescription(boolAttribute(optional), "Automatically remove collections that are not present in the backup."),
+					"filter_keys":             WithDescription(stringAttribute([]string{optional}), "Only restore data where the key matches a particular regular expression."),
+					"filter_values":           WithDescription(stringAttribute([]string{optional}), "Only restore data where the value matches a particular regular expression."),
+					"include_data":            WithDescription(stringAttribute([]string{optional}), "Restores only the data specified here."),
+					"exclude_data":            WithDescription(stringAttribute([]string{optional}), "Skips restoring the data specified here."),
+					"map_data":                WithDescription(stringAttribute([]string{optional}), "Specified when you want to restore source data into a different location"),
+					"replace_ttl":             WithDescription(stringAttribute([]string{optional}), "Sets a new expiration (time-to-live) value for the specified keys. Values being 'none' 'all' 'expired'"),
+					"replace_ttl_with":        WithDescription(stringAttribute([]string{optional}), "Updates the expiration for the keys."),
+					"status":                  WithDescription(stringAttribute([]string{computed}), "The status of the restore."),
 				},
 			},
-			"restore_times": schema.NumberAttribute{
-				Optional:            true,
-				MarkdownDescription: "Number of times the backup to be restored",
-			},
+			"restore_times": WithDescription(numberAttribute(optional), "Number of times the backup to be restored."),
 		},
 	}
 }
