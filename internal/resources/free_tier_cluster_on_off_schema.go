343980a1bca1cd7a99427c7d9a50fd59bd05b685
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-07 14:23:42 +0530

    adding with description function

--- internal/resources/free_tier_cluster_on_off_schema.go
+++ internal/resources/free_tier_cluster_on_off_schema.go
@@ -3,15 +3,16 @@ package resources
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 )
 
 func FreeTierClusterOnOffSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "Manages the on/off state resource of a Free Tier Cluster.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the organization."),
-			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the project."),
-			"cluster_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the cluster."),
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the organization."),
+			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the project."),
+			"cluster_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the cluster."),
 			"state":           WithDescription(stringAttribute([]string{required}), "The on/off state of the Free Tier Cluster."),
 		},
 	}
