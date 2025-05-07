782234e66b770796196da521d4ebeb78ca06647e
Karan Jain <76868354+karanjain-ops@users.noreply.github.com>
2025-05-08 00:24:55 +0530

    [AV-100869] feat: Added docs for project. (#314)

--- internal/resources/project_schema.go
+++ internal/resources/project_schema.go
@@ -8,18 +8,20 @@ import (
 
 func ProjectSchema() schema.Schema {
 	return schema.Schema{
+		MarkdownDescription: "Resource to create and manage a project in a Capella organization. Projects are used to organize and manage groups of Couchbase databases within organizations.",
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				Computed: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
+				MarkdownDescription: "The ID of the project created.",
 			},
-			"organization_id": stringAttribute([]string{required, requiresReplace}),
-			"name":            stringAttribute([]string{required}),
-			"description":     stringAttribute([]string{optional, computed}),
-			"if_match":        stringAttribute([]string{optional}),
-			"etag":            stringAttribute([]string{computed}),
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the organization."),
+			"name":            WithDescription(stringAttribute([]string{required}), "The name of the project (up to 128 characters)."),
+			"description":     WithDescription(stringAttribute([]string{optional, computed}), "A short description of the project (up to 256 characters)."),
+			"if_match":        WithDescription(stringAttribute([]string{optional}), "A precondition header that specifies the entity tag of a resource."),
+			"etag":            WithDescription(stringAttribute([]string{computed}), "The ETag header value returned by the server, used for optimistic concurrency control."),
 			"audit":           computedAuditAttribute(),
 		},
 	}
