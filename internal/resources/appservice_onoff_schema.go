7d37317edd951cc70f549c80588c685a48ba2db1
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-07 15:26:09 +0530

    adding with description function

--- internal/resources/appservice_onoff_schema.go
+++ internal/resources/appservice_onoff_schema.go
@@ -2,46 +2,17 @@ package resources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
 
 func AppServiceOnOffOnDemandSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "Manages the on-demand state of an app service. This resource is used to turn on/off the app service on-demand.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": schema.StringAttribute{
-				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				MarkdownDescription: "The GUID4 ID of the organization.",
-			},
-			"project_id": schema.StringAttribute{
-				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				MarkdownDescription: "The GUID4 ID of the project.",
-			},
-			"cluster_id": schema.StringAttribute{
-				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				MarkdownDescription: "The GUID4 ID of the cluster.",
-			},
-			"app_service_id": schema.StringAttribute{
-				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-				MarkdownDescription: "The GUID4 ID of the app service.",
-			},
-			"state": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "State of the app service on-demand. It can be on/off",
-			},
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the organization."),
+			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the project."),
+			"cluster_id":      WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the cluster."),
+			"app_service_id":  WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the app service."),
+			"state":           WithDescription(stringAttribute([]string{required}), "The state of the app service on-demand. It can be on/off."),
 		},
 	}
 }
