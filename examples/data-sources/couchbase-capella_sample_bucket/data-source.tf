c255e85b0e8d1294d3653e4f9198fc50a376e3ea
Karan Jain <76868354+karanjain-ops@users.noreply.github.com>
2025-05-08 00:17:41 +0530

    [AV-100866] feat: Added docs for sample bucket. (#317)

---
+++ examples/data-sources/couchbase-capella_sample_bucket/data-source.tf
@@ -0,0 +1,5 @@
+data "couchbase-capella_sample_buckets" "existing_sample_buckets" {
+  organization_id = "<organization_id>"
+  project_id      = "<project_id>"
+  cluster_id      = "<cluster_id>"
+}
---
+++ examples/resources/couchbase-capella_sample_bucket/import.sh
@@ -0,0 +1,1 @@
+terraform import couchbase-capella_sample_bucket.new_sample_bucket id=<bucket_id>,cluster_id=<cluster_id>,project_id=<project_id>,organization_id=<organization_id>
