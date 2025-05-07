ed6d4b476e766caf0759070b7d8584abf7a291d4
karanjain-ops <karan.jain@couchbase.com>
2025-05-07 17:46:55 +0530

    [AV-100870] fix: added top level description"

--- internal/resources/flush_bucket_schema.go
+++ internal/resources/flush_bucket_schema.go
@@ -7,6 +7,7 @@ import (
 
 func FlushBucketSchema() schema.Schema {
 	return schema.Schema{
+		MarkdownDescription: "Resources allows you to flush a Capella bucket. Flushing a bucket causes all documents in the bucket to be deleted by the system at the earliest. This operation can only be performed if the bucket has been configured with flushEnabled to true.",
 		Attributes: map[string]schema.Attribute{
 			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the Capella organization."),
 			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the project."),
