5595d56c75cbce23efb9e26c9503e37057e1781e
Karan Jain <76868354+karanjain-ops@users.noreply.github.com>
2025-05-08 00:27:08 +0530

    [AV-100870] feat: Added docs for flush bucket. (#313)

--- internal/resources/flush_bucket_schema.go
+++ internal/resources/flush_bucket_schema.go
@@ -7,11 +7,12 @@ import (
 
 func FlushBucketSchema() schema.Schema {
 	return schema.Schema{
+		MarkdownDescription: "Resources allows you to flush a Capella bucket. Flushing a bucket causes all documents in the bucket to be deleted by the system at the earliest. This operation can only be performed if the bucket has been configured with flushEnabled to true.",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)),
-			"project_id":      stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)),
-			"cluster_id":      stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)),
-			"bucket_id":       stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)),
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the Capella organization."),
+			"project_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the project."),
+			"cluster_id":      WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The GUID4 ID of the cluster."),
+			"bucket_id":       WithDescription(stringAttribute([]string{required, requiresReplace}, stringvalidator.LengthAtLeast(1)), "The ID of the bucket. It is the URL-compatible base64 encoding of the bucket name."),
 		},
 	}
 }
