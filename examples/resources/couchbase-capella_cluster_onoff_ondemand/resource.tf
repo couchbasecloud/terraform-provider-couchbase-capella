02775e2011597ead8da1ebc3ade2240c651c9cf1
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-09 00:27:33 +0530

    modifying the parameters

--- examples/resources/couchbase-capella_cluster_onoff_ondemand/resource.tf
+++ examples/resources/couchbase-capella_cluster_onoff_ondemand/resource.tf
@@ -2,6 +2,6 @@ resource "couchbase-capella_cluster_onoff_ondemand" "new_cluster_onoff_ondemand"
   organization_id            = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
   project_id                 = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
   cluster_id                 = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
-  state                      = "off"
+  state                      = "on"
   turn_on_linked_app_service = "true"
 }
