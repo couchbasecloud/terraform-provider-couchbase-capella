032d2ded420b77a51f69f844f924cfe9c7b33420
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-08 07:25:32 +0530

    formatting tf file

--- examples/resources/couchbase-capella_cluster_onoff_schedule/resource.tf
+++ examples/resources/couchbase-capella_cluster_onoff_schedule/resource.tf
@@ -4,66 +4,66 @@ resource "couchbase-capella_cluster_onoff_schedule" "new_cluster_onoff_schedule"
   cluster_id      = "cluster_id"
   timezone        = "ex:US/Hawaii"
   days = [
-  {
-    day   = "monday"
-    state = "custom"
-    from = {
-      hour   = 12
-      minute = 30
-    }
-    to = {
-      hour   = 14
-      minute = 30
-    }
-  },
-  {
-    day   = "tuesday"
-    state = "custom"
-    from = {
-      hour = 12
-    }
-    to = {
-      hour   = 19
-      minute = 30
-    }
-  },
-  {
-    day   = "wednesday"
-    state = "on"
-  },
-  {
-    day   = "thursday"
-    state = "custom"
-    from = {
-      hour   = 12
-      minute = 30
-    }
-  },
-  {
-    day   = "friday"
-    state = "custom"
-    from = {
+    {
+      day   = "monday"
+      state = "custom"
+      from = {
+        hour   = 12
+        minute = 30
+      }
+      to = {
+        hour   = 14
+        minute = 30
+      }
+    },
+    {
+      day   = "tuesday"
+      state = "custom"
+      from = {
+        hour = 12
+      }
+      to = {
+        hour   = 19
+        minute = 30
+      }
+    },
+    {
+      day   = "wednesday"
+      state = "on"
+    },
+    {
+      day   = "thursday"
+      state = "custom"
+      from = {
+        hour   = 12
+        minute = 30
+      }
+    },
+    {
+      day   = "friday"
+      state = "custom"
+      from = {
 
+      }
+      to = {
+        hour   = 12
+        minute = 30
+      }
+    },
+    {
+      day   = "saturday"
+      state = "custom"
+      from = {
+        hour   = 12
+        minute = 30
+      }
+      to = {
+        hour = 14
+      }
+    },
+    {
+      day   = "sunday"
+      state = "off"
     }
-    to = {
-      hour   = 12
-      minute = 30
-    }
-  },
-  {
-    day   = "saturday"
-    state = "custom"
-    from = {
-      hour   = 12
-      minute = 30
-    }
-    to = {
-      hour = 14
-    }
-  },
-  {
-    day   = "sunday"
-    state = "off"
-  }
-]
+  ]
 }
