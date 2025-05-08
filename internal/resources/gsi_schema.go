ad45257e53eaa1a334801255e9daa29c6922fc01
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-07 17:28:11 -0700

    Resource descriptions

--- internal/resources/gsi_schema.go
+++ internal/resources/gsi_schema.go
@@ -27,41 +27,56 @@ func GsiSchema() schema.Schema {
 		})
 
 	return schema.Schema{
+		MarkdownDescription: "Manages Query Indexes in Couchbase Capella",
 		Attributes: map[string]schema.Attribute{
-			"organization_id": stringAttribute([]string{required, requiresReplace}),
-			"project_id":      stringAttribute([]string{required, requiresReplace}),
-			"cluster_id":      stringAttribute([]string{required, requiresReplace}),
-			"bucket_name":     stringAttribute([]string{required, requiresReplace}),
-			"scope_name": stringDefaultAttribute(
+			"organization_id": WithDescription(stringAttribute([]string{required, requiresReplace}),
+				"The ID of the Capella organization."),
+			"project_id": WithDescription(stringAttribute([]string{required, requiresReplace}),
+				"The ID of the Capella project."),
+			"cluster_id": WithDescription(stringAttribute([]string{required, requiresReplace}),
+				"The ID of the Capella cluster where the index will be created."),
+			"bucket_name": WithDescription(stringAttribute([]string{required, requiresReplace}),
+				"The name of the bucket where the index will be created. Specifies the bucket part of the key space."),
+			"scope_name": WithDescription(stringDefaultAttribute(
 				"_default", optional, computed, useStateForUnknown, requiresReplace,
-			),
-			"collection_name": stringDefaultAttribute(
+			), "The name of the scope where the index will be created. Specifies the scope part of the key space. If unspecified, this will be the default scope."),
+			"collection_name": WithDescription(stringDefaultAttribute(
 				"_default", optional, computed, useStateForUnknown, requiresReplace,
-			),
-			"index_name":   stringAttribute([]string{optional, requiresReplace}),
-			"is_primary":   boolAttribute(optional),
-			"index_keys":   stringListAttribute(optional, requiresReplace),
-			"where":        stringAttribute([]string{optional, requiresReplace}),
-			"status":       stringAttribute([]string{computed, useStateForUnknown}),
-			"partition_by": stringListAttribute(optional, requiresReplace),
+			), "Specifies the collection part of the key space. If unspecified, this will be the default collection."),
+			"index_name": WithDescription(stringAttribute([]string{optional, requiresReplace}),
+				"The name of the index."),
+			"is_primary": WithDescription(boolAttribute(optional),
+				"Whether this is a primary index."),
+			"index_keys": WithDescription(stringListAttribute(optional, requiresReplace),
+				"List of document fields to index."),
+			"where": WithDescription(stringAttribute([]string{optional, requiresReplace}),
+				"WHERE clause for the index."),
+			"status": WithDescription(stringAttribute([]string{computed, useStateForUnknown}),
+				"The current status of the index. For example 'Created', 'Ready', etc."),
+			"partition_by": WithDescription(stringListAttribute(optional, requiresReplace),
+				"List of fields to partition the index by."),
 			"with": schema.SingleNestedAttribute{
-				Optional: true,
-				Computed: true,
-				Default:  objectdefault.StaticValue(defaultObject),
+				Optional:            true,
+				Computed:            true,
+				Default:             objectdefault.StaticValue(defaultObject),
+				MarkdownDescription: "Additional index configuration options.",
 				Attributes: map[string]schema.Attribute{
 					"defer_build": schema.BoolAttribute{
-						Optional:      true,
-						PlanModifiers: []planmodifier.Bool{custom_plan_modifiers.ImmutableBoolAttribute()},
+						Optional:            true,
+						MarkdownDescription: "If true, the index will not be built immediately after creation.",
+						PlanModifiers:       []planmodifier.Bool{custom_plan_modifiers.ImmutableBoolAttribute()},
 					},
 					"num_replica": schema.Int64Attribute{
-						Optional: true,
-						Computed: true,
+						Optional:            true,
+						Computed:            true,
+						MarkdownDescription: "Number of index replicas to create.",
 						PlanModifiers: []planmodifier.Int64{
 							int64planmodifier.UseStateForUnknown(),
 						},
 					},
 					"num_partition": schema.Int64Attribute{
-						Optional: true,
+						Optional:            true,
+						MarkdownDescription: "Number of partitions for the index.",
 						Validators: []validator.Int64{
 							int64validator.AtLeast(1),
 						},
@@ -69,7 +84,8 @@ func GsiSchema() schema.Schema {
 					},
 				},
 			},
-			"build_indexes": stringSetAttribute(optional),
+			"build_indexes": WithDescription(stringSetAttribute(optional),
+				"List of index names to build."),
 		},
 	}
 }
