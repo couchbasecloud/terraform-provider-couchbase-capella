a148f41b58612ac9959442d42d836afddcbcea1b
PaulomeeCb <paulomee.de@couchbase.com>
2025-05-08 11:14:42 -0700

    Resolving the examples

--- examples/resources/couchbase-capella_query_indexes/resource.tf
+++ examples/resources/couchbase-capella_query_indexes/resource.tf
@@ -37,33 +37,57 @@
     }
 
 # Example for deferred index build -
+locals {
+  index_template = templatefile("${path.module}/indexes.json", {
+    organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
+    project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+    cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+  })
+
+  decoded_template = jsondecode(local.index_template)
+  index_names      = [for idx, details in local.decoded_template.resource["couchbase-capella_indexes"] : details.index_name]
+}
+
+resource "couchbase-capella_query_indexes" "new_indexes" {
+  for_each        = jsondecode(local.index_template).resource["couchbase-capella_indexes"]
+  organization_id = each.value.organization_id
+  project_id      = each.value.project_id
+  cluster_id      = each.value.cluster_id
+  bucket_name     = each.value.bucket_name
+  scope_name      = each.value.scope_name
+  collection_name = each.value.collection_name
+  index_name      = each.value.index_name
+  index_keys      = each.value.index_keys
+  with = {
+    defer_build = each.value.with.defer_build
+  }
+}
 
 resource "couchbase-capella_query_indexes" "build_idx" {
+  organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
+  project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+  cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
+
+  bucket_name     = "api"
+  scope_name      = "metrics"
+  collection_name = "memory"
+
+  build_indexes = local.index_names
+
+  depends_on = [couchbase-capella_query_indexes.new_indexes]
+}
+
+data "couchbase-capella_query_index_monitor" "mon_indexes" {
   organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
   project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
   cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
   bucket_name     = "api"
   scope_name      = "metrics"
   collection_name = "memory"
-  build_indexes = = ["idx1","idx2", "idx3", "idx4", "idx5",]
+  indexes         = local.index_names
+
+  depends_on = [couchbase-capella_query_indexes.build_idx]
 }
-
-
-# couchbase-capella_query_indexes.new_indexes["idx1"] will be created -
- resource "couchbase-capella_query_indexes" "new_indexes" {
-    bucket_name     = "api"
-    cluster_id      = "ffffffff-aaaa-1414-eeee-000000000000"
-    collection_name = "memory"
-    index_keys      = ["field1"]
-    index_name      = "idx1"
-    organization_id = "ffffffff-aaaa-1414-eeee-000000000000"
-    project_id      = "ffffffff-aaaa-1414-eeee-000000000000"
-    scope_name      = "metrics"
-    with            = {
-      defer_build = true
-        }
-    }
-
 
 
 # Example for non-deferred index build -
