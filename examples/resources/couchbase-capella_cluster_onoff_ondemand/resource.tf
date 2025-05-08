c90ef85dfb3b0a80950c96ab89d20850f28a4b95
Saicharan Rachamadugu <saicharan.rachamadugu@couchbase.com>
2025-05-08 07:39:03 +0530

    updating examples with dummy values

--- examples/resources/couchbase-capella_cluster_onoff_ondemand/resource.tf
+++ examples/resources/couchbase-capella_cluster_onoff_ondemand/resource.tf
@@ -1,7 +1,7 @@
 resource "couchbase-capella_cluster_onoff_ondemand" "new_cluster_onoff_ondemand" {
-  organization_id            = "organization_id"
-  project_id                 = "project_id"
-  cluster_id                 = "cluster_id"
+  organization_id            = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
+  project_id                 = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
+  cluster_id                 = "aaaaa-bbbb-cccc-dddd-eeeeeeeee"
   state                      = "off"
   turn_on_linked_app_service = "true"
 }
