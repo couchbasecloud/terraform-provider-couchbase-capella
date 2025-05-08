1cebf1a0fb608ea4d57a6a0c2d63903b0fa75aa2
Saicharan Rachamadugu <102158954+SaicharanCB@users.noreply.github.com>
2025-05-08 22:10:20 +0530

    AV-100879 : Adding certificate docs (#320)

---
+++ examples/data-sources/couchbase-capella_certificate/data-source.tf
@@ -0,0 +1,5 @@
+data "couchbase-capella_certificate" "existing_certificate" {
+  organization_id = "aaaaaa-bbbbbcc-cccc-dddddddd"
+  project_id      = "aaaaaa-bbbbbcc-cccc-dddddddd"
+  cluster_id      = "aaaaaa-bbbbbcc-cccc-dddddddd"
+}
