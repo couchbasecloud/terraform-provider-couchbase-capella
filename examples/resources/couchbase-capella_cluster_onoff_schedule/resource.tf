c908937dfd4a13e6551b74ed7a77a5939b2d5ef1
Saicharan Rachamadugu <102158954+SaicharanCB@users.noreply.github.com>
2025-05-08 07:22:51 +0530

    Update resource.tf

--- examples/resources/couchbase-capella_cluster_onoff_schedule/resource.tf
+++ examples/resources/couchbase-capella_cluster_onoff_schedule/resource.tf
@@ -2,21 +2,68 @@ resource "couchbase-capella_cluster_onoff_schedule" "new_cluster_onoff_schedule"
   organization_id = "organization_id"
   project_id      = "project_id"
   cluster_id      = "cluster_id"
-  timezone        = "timezone(ex:US/Hawaii)"
+  timezone        = "ex:US/Hawaii"
   days = [
-    for day in var.days : {
-      state = "off"
-      day   = "Monday"
-      from =  {
-                  hour   = 12
-                  minute = 30
-                }
-       to = {
-                  hour   = 14
-                  minute = 30
-                }
-      from  = "from time"
-      to    = "to time"
+  {
+    day   = "monday"
+    state = "custom"
+    from = {
+      hour   = 12
+      minute = 30
     }
-  ]
+    to = {
+      hour   = 14
+      minute = 30
+    }
+  },
+  {
+    day   = "tuesday"
+    state = "custom"
+    from = {
+      hour = 12
+    }
+    to = {
+      hour   = 19
+      minute = 30
+    }
+  },
+  {
+    day   = "wednesday"
+    state = "on"
+  },
+  {
+    day   = "thursday"
+    state = "custom"
+    from = {
+      hour   = 12
+      minute = 30
+    }
+  },
+  {
+    day   = "friday"
+    state = "custom"
+    from = {
+
+    }
+    to = {
+      hour   = 12
+      minute = 30
+    }
+  },
+  {
+    day   = "saturday"
+    state = "custom"
+    from = {
+      hour   = 12
+      minute = 30
+    }
+    to = {
+      hour = 14
+    }
+  },
+  {
+    day   = "sunday"
+    state = "off"
+  }
+]
 }
