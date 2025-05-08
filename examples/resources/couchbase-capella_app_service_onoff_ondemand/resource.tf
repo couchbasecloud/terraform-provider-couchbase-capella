9010abbab64fc95d738697fd5629200e9bf21803
Saicharan Rachamadugu <102158954+SaicharanCB@users.noreply.github.com>
2025-05-08 07:27:42 +0530

    Update examples/resources/couchbase-capella_app_service_onoff_ondemand/resource.tf
    Co-authored-by: Paulomee De <124640240+PaulomeeCb@users.noreply.github.com>

--- examples/resources/couchbase-capella_app_service_onoff_ondemand/resource.tf
+++ examples/resources/couchbase-capella_app_service_onoff_ondemand/resource.tf
@@ -3,5 +3,5 @@ resource "couchbase-capella_app_service_onoff_ondemand" "new_app_service_onoff_o
   project_id      = "project_id"
   cluster_id      = "cluster_id"
   app_service_id  = "app_service_id"
-  state           = "state"
+  state           = "off"
 }
