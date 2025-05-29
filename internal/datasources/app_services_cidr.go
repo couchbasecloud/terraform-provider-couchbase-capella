4982698e564293ee2a63198282aa8b79f9b9f118
Hitesh Walia <hitesh.walia@couchbase.com>
2025-05-29 16:48:37 -0700

    datasource metadata

--- internal/datasources/app_services_cidr.go
+++ internal/datasources/app_services_cidr.go
@@ -1,12 +1,13 @@
 package datasources
 
 import (
+	"context"
 	providerschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 )
 
 var (
-	_ datasource.DataSource              = (*AppServiceCidrs)(nil)
+	_ datasource.DataSource = (*AppServiceCidrs)(nil)
 	_ datasource.DataSourceWithConfigure = (*AppServiceCidrs)(nil)
 )
 
@@ -16,4 +17,10 @@ type AppServiceCidrs struct {
 
 func NewAppServiceCidrs() datasource.DataSource {
 	return &AppServiceCidrs{}
+}
+
+func (a *AppServiceCidrs) Metadata(
+	_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse,
+) {
+	resp.TypeName = req.ProviderTypeName + "_app_service_cidrs"
 }
