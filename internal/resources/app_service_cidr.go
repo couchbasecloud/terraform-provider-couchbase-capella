85c1f4d1254cb7053bcf1da1a5b0905cba890281
Hitesh Walia <hitesh.walia@couchbase.com>
2025-05-29 16:08:16 -0700

    resource configure

--- internal/resources/app_service_cidr.go
+++ internal/resources/app_service_cidr.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"context"
+	"fmt"
 	providerschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
@@ -27,4 +28,22 @@ func (a *AppServiceCidr) Metadata(_ context.Context, req resource.MetadataReques
 func (a *AppServiceCidr) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
 
 	// TODO
+}
+
+func (a *AppServiceCidr) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	data, ok := req.ProviderData.(*providerschema.Data)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *ProviderSourceData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	a.Data = data
 }
