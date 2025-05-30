66383baffe168dc3cbc73e0c7487dc9421f2427c
Hitesh Walia <hitesh.walia@couchbase.com>
2025-05-30 15:30:59 -0700

    corrections

--- internal/resources/app_service_cidr.go
+++ internal/resources/app_service_cidr.go
@@ -25,7 +25,7 @@ func NewAppServiceCidr() resource.Resource {
 }
 
 func (a *AppServiceCidr) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_project"
+	resp.TypeName = req.ProviderTypeName + "_app_services_cidr"
 }
 
 func (a *AppServiceCidr) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
@@ -99,6 +99,8 @@ func (a *AppServiceCidr) Create(ctx context.Context, req resource.CreateRequest,
 		return
 	}
 
+	// TODO set computed attributes to null
+
 	// TODO get latest status of the CIDR
 
 	// TODO save state
@@ -110,12 +112,7 @@ func (a *AppServiceCidr) Read(ctx context.Context, req resource.ReadRequest, res
 }
 
 func (a *AppServiceCidr) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	// Couchbase Capella's v4 does not support a PUT endpoint for app services cidr.
-	// This resource can only be created, read and deleted.
-	//
-	// Note: In this situation, terraform apply will default to deleting and executing a new create.
-	// The update implementation should simply be left empty.
-	// https://developer.hashicorp.com/terraform/plugin/framework/resources/update
+	// TODO return error
 }
 
 func (a *AppServiceCidr) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
