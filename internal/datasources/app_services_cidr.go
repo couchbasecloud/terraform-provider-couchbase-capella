56b449b237921f1f02108c2b19ea8104a3666df9
Mohammed Madi <madim0044@gmail.com>
2025-06-02 15:07:43 +0100

    Add list allowed cidrs path and schema

--- internal/datasources/app_services_cidr.go
+++ internal/datasources/app_services_cidr.go
@@ -2,8 +2,12 @@ package datasources
 
 import (
 	"context"
+	"fmt"
+	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api"
 	providerschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"net/http"
 )
 
 var (
@@ -26,7 +30,55 @@ func (a *AppServiceCidrs) Metadata(
 }
 
 func (a *AppServiceCidrs) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	// TODO
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Retrieves the allowlists details for a Capella App Service.",
+		Attributes: map[string]schema.Attribute{
+			"data": schema.ListNestedAttribute{
+				MarkdownDescription: "The list of allowed CIDRs on an App Service. ",
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The ID of the allowed CIDR.",
+						},
+						"organization_id": computedStringAttribute,
+						"project_id":      computedStringAttribute,
+						"cluster_id":      computedStringAttribute,
+						"cidr": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The trusted CIDR to allow the database connections from.",
+						},
+						"comment": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "The trusted CIDR to allow the database connections from.",
+						},
+						"expires_at": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "An RFC3339 timestamp determining when the allowed CIDR will expire. If this field is empty/omitted then the allowed CIDR is permanent and will never automatically expire.",
+						},
+						"audit": computedAuditAttribute,
+					},
+				},
+			},
+		},
+	}
+}
+
+// listAllowLists executes calls to the list allowlist endpoint. It handles pagination and
+// returns a slice of individual allowlists responses retrieved from multiple pages.
+func (d *AllowLists) listAllowedCIDRs(ctx context.Context, organizationId, projectId, clusterId, appServiceId string) ([]api.GetAllowListResponse, error) {
+	url := fmt.Sprintf(
+		"%s/v4/organizations/%s/projects/%s/clusters/%s/appservices/%s/allowedcidrs",
+		d.HostURL,
+		organizationId,
+		projectId,
+		clusterId,
+		appServiceId,
+	)
+
+	cfg := api.EndpointCfg{Url: url, Method: http.MethodGet, SuccessStatus: http.StatusOK}
+	return api.GetPaginated[[]api.GetAllowListResponse](ctx, d.Client, d.Token, cfg, api.SortById)
 }
 
 func (a *AppServiceCidrs) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
