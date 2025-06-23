e4667c39da5f0dea448b3f697998c6bcf08b83c4
Mohammed Madi <madim0044@gmail.com>
2025-06-23 17:21:37 +0100

    Add app endpoint schema file and resources schema

--- internal/resources/app_endpoint_schema.go
+++ internal/resources/app_endpoint_schema.go
@@ -5,7 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// AppEndpointSchema defines the schema for the AppEndpoint resource.
+// AppEndpointSchema defines the schema for the AppEndpoint resource
 func AppEndpointSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "This resource allows you to manage an App Endpoint configuration for a Couchbase Capella App Service.",
@@ -13,23 +13,30 @@ func AppEndpointSchema() schema.Schema {
 			"organization_id":    WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the organization."),
 			"project_id":         WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the project."),
 			"cluster_id":         WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the cluster."),
-			"app_service_id":     WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the App Service."),
-			"bucket":             WithDescription(stringAttribute([]string{required, requiresReplace}), "The name of the bucket associated with this App Endpoint."),
-			"name":               WithDescription(stringAttribute([]string{required, requiresReplace}), "The name of the App Endpoint."),
-			"user_xattr_key":     WithDescription(stringAttribute([]string{optional}), "The user extended attribute key for the App Endpoint."),
-			"delta_sync_enabled": WithDescription(boolAttribute(optional), "States whether delta sync is enabled for this App Endpoint."),
-			"scopes": schema.MapNestedAttribute{
+			"bucket":             WithDescription(stringAttribute([]string{required, requiresReplace}), "The name of the bucket associated with this app endpoint."),
+			"name":               WithDescription(stringAttribute([]string{required, requiresReplace}), "The name of the app endpoint."),
+			"user_xattr_key":     WithDescription(stringAttribute([]string{optional}), "The user extended attribute key for the app endpoint."),
+			"delta_sync_enabled": WithDescription(boolAttribute(optional), "Whether delta sync is enabled for this app endpoint."),
+			"scopes": schema.SingleNestedAttribute{
 				Optional:            true,
-				MarkdownDescription: "Configuration for scopes within the App Endpoint.",
-				NestedObject: schema.NestedAttributeObject{
-					Attributes: map[string]schema.Attribute{
-						"collections": schema.MapNestedAttribute{
-							Optional:            true,
-							MarkdownDescription: "Configuration for collections within this scope.",
-							NestedObject: schema.NestedAttributeObject{
+				MarkdownDescription: "Configuration for scopes within the app endpoint.",
+				Attributes: map[string]schema.Attribute{
+					"_default": schema.SingleNestedAttribute{
+						Optional:            true,
+						MarkdownDescription: "Configuration for the default scope.",
+						Attributes: map[string]schema.Attribute{
+							"collections": schema.SingleNestedAttribute{
+								Optional:            true,
+								MarkdownDescription: "Configuration for collections within the default scope.",
 								Attributes: map[string]schema.Attribute{
-									"access_control_function": WithDescription(stringAttribute([]string{optional}), "The Javascript function that is used to specify the access control policies to be applied to documents in this collection. Every document update is processed by this function."),
-									"import_filter":           WithDescription(stringAttribute([]string{optional}), "The JavaScript function used to filter which documents in the collection that are to be imported by the App Endpoint."),
+									"_default": schema.SingleNestedAttribute{
+										Optional:            true,
+										MarkdownDescription: "Configuration for the default collection.",
+										Attributes: map[string]schema.Attribute{
+											"access_control_function": WithDescription(stringAttribute([]string{optional}), "The access control function for the default collection."),
+											"import_filter":           WithDescription(stringAttribute([]string{optional}), "The import filter for the default collection."),
+										},
+									},
 								},
 							},
 						},
@@ -38,7 +45,7 @@ func AppEndpointSchema() schema.Schema {
 			},
 			"cors": schema.SingleNestedAttribute{
 				Optional:            true,
-				MarkdownDescription: "CORS configuration for the App Endpoint.",
+				MarkdownDescription: "CORS configuration for the app endpoint.",
 				Attributes: map[string]schema.Attribute{
 					"origin": schema.ListAttribute{
 						Optional:            true,
@@ -55,58 +62,23 @@ func AppEndpointSchema() schema.Schema {
 						ElementType:         types.StringType,
 						MarkdownDescription: "List of allowed headers for CORS.",
 					},
-					"max_age": schema.Int64Attribute{
-						Optional:            true,
-						MarkdownDescription: "Maximum age, in seconds, for CORS preflight requests in seconds.",
-					},
-					"disabled": schema.BoolAttribute{Optional: true, Description: "Disables/Enables CORS for this App Endpoint."},
+					"disabled": WithDescription(boolAttribute(optional), "Whether CORS is disabled for this app endpoint."),
 				},
 			},
 			"oidc": schema.ListNestedAttribute{
 				Optional:            true,
-				MarkdownDescription: "List of OIDC configurations for the App Endpoint.",
+				MarkdownDescription: "List of OIDC configurations for the app endpoint.",
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"issuer":         WithDescription(stringAttribute([]string{required}), "The OIDC issuer URL."),
-						"register":       WithDescription(boolAttribute(optional), "States whether to register the OIDC client."),
+						"register":       WithDescription(boolAttribute(optional), "Whether to register the OIDC client."),
 						"client_id":      WithDescription(stringAttribute([]string{required}), "The OIDC client ID."),
 						"user_prefix":    WithDescription(stringAttribute([]string{optional}), "The user prefix for OIDC users."),
 						"discovery_url":  WithDescription(stringAttribute([]string{optional}), "The OIDC discovery URL."),
 						"username_claim": WithDescription(stringAttribute([]string{optional}), "The username claim for OIDC."),
 						"roles_claim":    WithDescription(stringAttribute([]string{optional}), "The roles claim for OIDC."),
-						"provider_id":    WithDescription(stringAttribute([]string{computed}), "The OIDC provider ID."),
-						"is_default":     WithDescription(boolAttribute(computed), "States whether this is the default OIDC provider."),
 					},
 				},
-			},
-			"require_resync": schema.SingleNestedAttribute{
-				Computed:            true,
-				MarkdownDescription: "List of collections that require resync, keyed by scope.",
-				Attributes: map[string]schema.Attribute{
-					"scope_name": schema.SingleNestedAttribute{
-						Computed:            true,
-						MarkdownDescription: "The scope's name.",
-						Attributes: map[string]schema.Attribute{
-							"items": schema.ListAttribute{
-								Computed:            true,
-								ElementType:         types.StringType,
-								MarkdownDescription: "List of collections that require resync under this scope.",
-							},
-						},
-					},
-				},
-			},
-			"admin_url": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "The admin URL for the App Endpoint.",
-			},
-			"metrics_url": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "The metrics URL for the App Endpoint.",
-			},
-			"public_url": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "The public URL for the App Endpoint.",
 			},
 		},
 	}
--- internal/schema/app_endpoint.go
+++ internal/schema/app_endpoint.go
@@ -1,170 +1,56 @@
 package schema
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// AppEndpoint represents the Terraform schema for an app endpoint configuration.
+// AppEndpoint represents the Terraform schema for an app endpoint configuration
 type AppEndpoint struct {
-	// OrganizationId is the ID of the organization to which the App Endpoint belongs.
-	OrganizationId types.String `tfsdk:"organization_id"`
-
-	// ProjectId is the ID of the project to which the App Endpoint belongs.
-	ProjectId types.String `tfsdk:"project_id"`
-
-	// ClusterId is the ID of the cluster to which the App Endpoint belongs.
-	ClusterId types.String `tfsdk:"cluster_id"`
-
-	// AppServiceId is the ID of the App Service to which the App Endpoint belongs.
-	AppServiceId types.String `tfsdk:"app_service_id"`
-
-	// Bucket The Capella Cluster backing bucket for the App Endpoint.
-	Bucket types.String `tfsdk:"bucket"`
-
-	// Name is the name of the App Endpoint.
-	Name types.String `tfsdk:"name"`
-
-	// UserXattrKey is the key used for user extended attributes in the App Endpoint.
-	UserXattrKey types.String `tfsdk:"user_xattr_key"`
-
-	// DeltaSyncEnabled Indicates whether Delta Sync is enabled for the App Endpoint.
-	DeltaSyncEnabled types.Bool `tfsdk:"delta_sync_enabled"`
-
-	// Scopes defines the scopes and collections for the App Endpoint.
-	Scopes types.Map `tfsdk:"scopes"`
-
-	// Cors configures cross origin resource sharing (CORS) for the App Endpoint.
-	Cors types.Object `tfsdk:"cors"`
-
-	// Oidc is a list of OIDC provider configurations for the App Endpoint.
-	Oidc types.List `tfsdk:"oidc"`
-
-	// RequireResync is a map of scopes to a list of collection names that require resync.
-	RequireResync types.Map `tfsdk:"require_resync"`
-
-	// AdminURL A URL for the admin API used for the administration of App Endpoints. For more information, read the [Capella App Services Admin API Reference](https://docs.couchbase.com/cloud/app-services/references/rest-api-introduction.html#:~:text=Capella%20App%20Services%20Admin%20API%20Reference)
-	AdminURL types.String `tfsdk:"admin_url"`
-
-	// MetricsURL A URL for the metrics API used for monitoring App Services performance metrics. For more information, read the [Capella App Services Metrics API Reference](https://docs.couchbase.com/cloud/app-services/references/rest_api_metric.html)
-	MetricsURL types.String `tfsdk:"metrics_url"`
-
-	// PublicURL A URL for the public API used for access to functions for data access and manipulation. For more information, read the [Capella App Services Public API Reference](https://docs.couchbase.com/cloud/app-services/references/rest_api_public.html)
-	PublicURL types.String `tfsdk:"public_url"`
+	Bucket           types.String    `tfsdk:"bucket"`
+	Name             types.String    `tfsdk:"name"`
+	UserXattrKey     types.String    `tfsdk:"user_xattr_key"`
+	DeltaSyncEnabled types.Bool      `tfsdk:"delta_sync_enabled"`
+	Scopes           types.Object    `tfsdk:"scopes"`
+	Cors             AppEndpointCors `tfsdk:"cors"`
+	Oidc             AppEndpointOidc `tfsdk:"oidc"`
 }
 
-// ScopesConfig maps scope name to a list of collection names.
-type (
-	// AppEndpointScopes represents a map of scope names to collections.
-	AppEndpointScopes map[types.String]AppEndpointScopeConfig
-	// RequireResync
-	RequireResync          types.MapType
-	AppEndpointScopeConfig struct {
-		// Collections is a map of collections names to their configurations.
-		Collections map[types.String]AppEndpointCollection `tfsdk:"collections"` // Collection-specific config options.
-	}
-)
+// AppEndpointScopes represents the scopes configuration within an app endpoint
+type AppEndpointScopes struct {
+	Default types.Object `tfsdk:"_default"`
+}
 
-// AppEndpointCollection represents a collection configuration.
-type AppEndpointCollection struct {
-	// AccessControlFunction The Javascript function that is used to specify the access control policies to be applied to documents in this collection.
-	// Every document update is processed by this function. The default access control function is 'function(doc){channel(doc.channels);}'
-	// for the default collection and 'function(doc){channel(collectionName);}' for named collections.
+// AppEndpointDefaultScope represents the default scope configuration
+type AppEndpointDefaultScope struct {
+	Collections types.Object `tfsdk:"collections"`
+}
+
+// AppEndpointCollections represents the collections configuration within a scope
+type AppEndpointCollections struct {
+	Default types.Object `tfsdk:"_default"`
+}
+
+// AppEndpointDefaultCollection represents the default collection configuration
+type AppEndpointDefaultCollection struct {
 	AccessControlFunction types.String `tfsdk:"access_control_function"`
-	// ImportFilter The Javascript function used to specify the documents in this collection that are to be imported by the App Endpoint. By default, all documents in corresponding collection are imported.
-	ImportFilter types.String `tfsdk:"import_filter"`
+	ImportFilter          types.String `tfsdk:"import_filter"`
 }
 
-// AppEndpointCors represents the CORS configuration for an app endpoint.
+// AppEndpointCors represents the CORS configuration for an app endpoint
 type AppEndpointCors struct {
-	// Origin List of allowed origins, use ['*'] to allow access from everywhere
-	Origin []types.String `tfsdk:"origin"`
-	// LoginOrigin List of allowed login origins
-	LoginOrigin []types.String `tfsdk:"login_origin"`
-	// Headers List of allowed headers
-	Headers []types.String `tfsdk:"headers"`
-	// MaxAge Specifies the duration (in seconds) for which the results of a preflight request can be cached.
-	MaxAge types.Int64 `tfsdk:"max_age"`
-	// Disabled indicated whether CORS is disabled.
-	Disabled types.Bool `tfsdk:"disabled"`
+	Origin      types.List `tfsdk:"origin"`
+	LoginOrigin types.List `tfsdk:"login_origin"`
+	Headers     types.List `tfsdk:"headers"`
+	Disabled    types.Bool `tfsdk:"disabled"`
 }
 
-// AppEndpointOidc represents an OIDC configuration within an app endpoint.
+// AppEndpointOidc represents an OIDC configuration within an app endpoint
 type AppEndpointOidc struct {
-	// Issuer The URL for the OpenID Connect issuer.
-	Issuer types.String `tfsdk:"issuer"`
-	// Register Indicates whether to register a new App Service user account when a user logs in using OpenID Connect.
-	Register types.Bool `tfsdk:"register"`
-	// ClientId The OpenID Connect provider client ID.
-	ClientId types.String `tfsdk:"client_id"`
-	// UserPrefix Username prefix for all users created for this provider
-	UserPrefix types.String `tfsdk:"user_prefix"`
-	// DiscoveryUrl The URL for the non-standard discovery endpoint.
-	DiscoveryUrl types.String `tfsdk:"discovery_url"`
-	// UsernameClaim Allows a different OpenID Connect field to be specified instead of the Subject (sub).
+	Issuer        types.String `tfsdk:"issuer"`
+	Register      types.Bool   `tfsdk:"register"`
+	ClientId      types.String `tfsdk:"client_id"`
+	UserPrefix    types.String `tfsdk:"user_prefix"`
+	DiscoveryUrl  types.String `tfsdk:"discovery_url"`
 	UsernameClaim types.String `tfsdk:"username_claim"`
-	// RolesClaim If set, the value(s) of the given OpenID Connect authentication token claim will be added to the user's roles.
-	// The value of this claim in the OIDC token must be either a string or an array of strings, any other type will result in an error.
-	RolesClaim types.String `tfsdk:"roles_claim"`
-	// ProviderId UUID of the provider.
-	ProviderId types.String `tfsdk:"provider_id"`
-	// IsDefault Indicates whether this is the default OpenID Connect provider.
-	IsDefault types.Bool `tfsdk:"is_default"`
-}
-
-func (a AppEndpoint) AttributeTypes() map[string]attr.Type {
-	return map[string]attr.Type{
-		"organization_id":    types.StringType,
-		"project_id":         types.StringType,
-		"cluster_id":         types.StringType,
-		"app_service_id":     types.StringType,
-		"bucket":             types.StringType,
-		"name":               types.StringType,
-		"user_xattr_key":     types.StringType,
-		"delta_sync_enabled": types.BoolType,
-		"scopes":             types.MapType{ElemType: types.ObjectType{AttrTypes: AppEndpointScopeConfig{}.AttributeTypes()}},
-		"cors":               types.ObjectType{AttrTypes: AppEndpointCors{}.AttributeTypes()},
-		"oidc":               types.ListType{ElemType: types.ObjectType{AttrTypes: AppEndpointOidc{}.AttributeTypes()}},
-		"require_resync":     types.MapType{ElemType: types.ListType{ElemType: types.StringType}},
-		"admin_url":          types.StringType,
-		"metrics_url":        types.StringType,
-		"public_url":         types.StringType,
-	}
-}
-
-func (s AppEndpointScopeConfig) AttributeTypes() map[string]attr.Type {
-	return map[string]attr.Type{
-		"collections": types.MapType{ElemType: types.ObjectType{AttrTypes: AppEndpointCollection{}.AttributeTypes()}},
-	}
-}
-
-func (o AppEndpointOidc) AttributeTypes() map[string]attr.Type {
-	return map[string]attr.Type{
-		"issuer":         types.StringType,
-		"register":       types.BoolType,
-		"client_id":      types.StringType,
-		"user_prefix":    types.StringType,
-		"discovery_url":  types.StringType,
-		"username_claim": types.StringType,
-		"roles_claim":    types.StringType,
-		"provider_id":    types.StringType,
-		"is_default":     types.BoolType,
-	}
-}
-
-func (c AppEndpointCors) AttributeTypes() map[string]attr.Type {
-	return map[string]attr.Type{
-		"origin":       types.ListType{ElemType: types.StringType},
-		"login_origin": types.ListType{ElemType: types.StringType},
-		"headers":      types.ListType{ElemType: types.StringType},
-		"max_age":      types.Int64Type,
-		"disabled":     types.BoolType,
-	}
-}
-
-func (c AppEndpointCollection) AttributeTypes() map[string]attr.Type {
-	return map[string]attr.Type{
-		"access_control_function": types.StringType,
-		"import_filter":           types.StringType,
-	}
+	RolesClaim    types.String `tfsdk:"roles_claim"`
 }
