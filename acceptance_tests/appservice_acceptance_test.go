f712d50764441bd274fc83772fe5a669dd5ca44a
Hitesh Walia <hitesh.walia@couchbase.com>
2025-06-30 12:49:11 -0700

    fix checks

--- acceptance_tests/appservice_acceptance_test.go
+++ acceptance_tests/appservice_acceptance_test.go
@@ -20,7 +20,6 @@ func TestAppServiceResource(t *testing.T) {
 			{
 				Config: testAccAppServiceResourceConfig(resourceName),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceReference, "name", resourceReference),
 					resource.TestCheckResourceAttr(resourceReference, "compute.cpu", "2"),
 					resource.TestCheckResourceAttr(resourceReference, "compute.ram", "4"),
 				),
