c307aa1e29b5b5b716b60275b3dc89b0361a0649
Hitesh Walia <hitesh.walia@couchbase.com>
2025-07-24 11:55:47 -0700

    update create handler

--- internal/resources/app_endpoint.go
+++ internal/resources/app_endpoint.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api"
-	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api/app_endpoints"
-	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api/appservice"
-	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"net/http"
 
-	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/errors"
+	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api/appservice"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+
+	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api"
+	"github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/api/app_endpoints"
 	providerschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 )
 
@@ -35,7 +37,9 @@ func NewAppEndpoint() resource.Resource {
 }
 
 // ImportState imports a remote AppEndpoint app service that is not created by Terraform.
-func (a *AppEndpoint) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+func (a *AppEndpoint) ImportState(
+	ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse,
+) {
 	// Retrieve import name and save to id attribute
 	resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
 }
@@ -75,57 +79,85 @@ func (a *AppEndpoint) Create(ctx context.Context, req resource.CreateRequest, re
 	var plan providerschema.AppEndpoint
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	if err := a.validateCreateAppEndpointRequest(plan); err != nil {
-		resp.Diagnostics.AddError(
-			"Error parsing create app endpoint request",
-			"Could not create app endpoint: "+err.Error(),
-		)
-		return
-	}
-	var scopes app_endpoints.ScopesConfig
-	diags = plan.Scopes.ElementsAs(ctx, &scopes, false)
-	if diags.HasError() {
-		resp.Diagnostics.Append(diags...)
-		return
+	scope := plan.Scope.ValueString()
+	sc := make(map[string]types.Object)
+	plan.Collections.ElementsAs(ctx, &sc, false)
+
+	nestedMap := make(map[string]map[string]map[string]app_endpoints.AppEndpointCollection)
+	nestedMap[scope] = make(map[string]map[string]app_endpoints.AppEndpointCollection)
+
+	fieldSetters := map[string]func(*app_endpoints.AppEndpointCollection, string){
+		"access_control_function": func(
+			c *app_endpoints.AppEndpointCollection, val string,
+		) {
+			c.AccessControlFunction = &val
+		},
+		"import_filter": func(c *app_endpoints.AppEndpointCollection, val string) { c.ImportFilter = &val },
 	}
 
-	// Create the app endpoint using the API
+	for col, obj := range sc {
+		if _, ok := nestedMap[scope]["collections"]; !ok {
+			nestedMap[scope]["collections"] = make(map[string]app_endpoints.AppEndpointCollection)
+		}
+
+		attr := obj.Attributes()
+		endpointCollection := nestedMap[scope]["collections"][col]
+
+		for name, value := range attr {
+			fieldSetters[name](&endpointCollection, value.String())
+		}
+
+		nestedMap[scope]["collections"][col] = endpointCollection
+	}
+
 	createAppEndpointRequest := app_endpoints.CreateAppEndpointRequest{
 		Bucket:           plan.Bucket.ValueString(),
 		Name:             plan.Name.ValueString(),
 		DeltaSyncEnabled: plan.DeltaSyncEnabled.ValueBool(),
-		Scopes:           scopes,
+		Scopes:           nestedMap,
+		Cors: &app_endpoints.AppEndpointCors{
+			Origin:      providerschema.BaseStringsToStrings(plan.Cors.Origin),
+			LoginOrigin: providerschema.BaseStringsToStrings(plan.Cors.LoginOrigin),
+			Headers:     providerschema.BaseStringsToStrings(plan.Cors.Headers),
+			MaxAge:      plan.Cors.MaxAge.ValueInt64Pointer(),
+			Disabled:    plan.Cors.Disabled.ValueBoolPointer(),
+		},
+	}
+	if len(plan.Oidc) > 0 {
+		createAppEndpointRequest.Oidc = make([]app_endpoints.AppEndpointOidc, len(plan.Oidc))
+		for i, oidc := range plan.Oidc {
+			createAppEndpointRequest.Oidc[i] = app_endpoints.AppEndpointOidc{
+				Issuer:        oidc.Issuer.ValueString(),
+				ClientId:      oidc.ClientId.ValueString(),
+				UserPrefix:    oidc.UserPrefix.ValueStringPointer(),
+				DiscoveryUrl:  oidc.DiscoveryUrl.ValueStringPointer(),
+				UsernameClaim: oidc.UsernameClaim.ValueStringPointer(),
+				RolesClaim:    oidc.RolesClaim.ValueStringPointer(),
+				Register:      oidc.Register.ValueBoolPointer(),
+			}
+		}
 	}
 
-	if !plan.Cors.IsNull() && !plan.Cors.IsUnknown() {
-		cors := app_endpoints.AppEndpointCors{}
-		diags = plan.Cors.As(ctx, &cors, basetypes.ObjectAsOptions{})
-		if diags.HasError() {
-			resp.Diagnostics.Append(diags...)
-			return
-		}
-		createAppEndpointRequest.Cors = &cors
+	diags = resp.State.Set(ctx, initComputedAttributesToNull(plan))
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
-	if !(plan.Oidc.IsNull() || plan.Oidc.IsUnknown()) {
-		oidc := []app_endpoints.AppEndpointOidc{}
-		diags = plan.Oidc.ElementsAs(ctx, &oidc, false)
-		if diags.HasError() {
-			resp.Diagnostics.Append(diags...)
-		}
-		createAppEndpointRequest.Oidc = oidc
-	}
+	//if jsonData, err := json.MarshalIndent(createAppEndpointRequest, "", "  "); err == nil {
+	//	fmt.Printf("###DEBUG### createAppEndpointRequest: %s\n", string(jsonData))
+	//}
 
 	var organizationId = plan.OrganizationId.ValueString()
 	var projectId = plan.ProjectId.ValueString()
 	var clusterId = plan.ClusterId.ValueString()
+	var appServiceId = plan.AppServiceId.ValueString()
 
-	url := fmt.Sprintf("%s/v4/organizations/%s/projects/%s/clusters/%s/appservices", a.HostURL, organizationId, projectId, clusterId)
+	url := fmt.Sprintf("%s/v4/organizations/%s/projects/%s/clusters/%s/appservices/%s/appEndpoints", a.HostURL, organizationId, projectId, clusterId, appServiceId)
 	cfg := api.EndpointCfg{Url: url, Method: http.MethodPost, SuccessStatus: http.StatusCreated}
 
 	response, err := a.Client.ExecuteWithRetry(
@@ -153,118 +185,154 @@ func (a *AppEndpoint) Create(ctx context.Context, req resource.CreateRequest, re
 		return
 	}
 
-	diags = resp.State.Set(ctx, initializeAppEndpointWithPlan(plan))
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	diags = resp.State.Set(ctx, plan)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	// TODO refresh state
+	//diags = resp.State.Set(ctx, plan)
+	//resp.Diagnostics.Append(diags...)
+	//if resp.Diagnostics.HasError() {
+	//	return
+	//}
 }
 
 // validateCreateAppEndpointRequest validates the required fields for creating an app endpoint.
-func (a *AppEndpoint) validateCreateAppEndpointRequest(plan providerschema.AppEndpoint) error {
-	// Validate required IDs
-	if plan.OrganizationId.IsNull() || plan.OrganizationId.IsUnknown() {
-		return errors.ErrOrganizationIdCannotBeEmpty
-	}
-	if plan.ProjectId.IsNull() || plan.ProjectId.IsUnknown() {
-		return errors.ErrProjectIdCannotBeEmpty
-	}
-	if plan.ClusterId.IsNull() || plan.ClusterId.IsUnknown() {
-		return errors.ErrClusterIdCannotBeEmpty
-	}
-	if plan.AppServiceId.IsNull() || plan.AppServiceId.IsUnknown() {
-		return errors.ErrAppServiceIdCannotBeEmpty
-	}
-
-	// Validate required bucket name
-	if plan.Bucket.IsNull() || plan.Bucket.IsUnknown() {
-		return fmt.Errorf("bucket name cannot be empty")
-	}
-	if !providerschema.IsTrimmed(plan.Bucket.ValueString()) {
-		return fmt.Errorf("bucket name %s", errors.ErrNotTrimmed)
-	}
-
-	// Validate required endpoint name
-	if plan.Name.IsNull() || plan.Name.IsUnknown() {
-		return fmt.Errorf("app endpoint name cannot be empty")
-	}
-	if !providerschema.IsTrimmed(plan.Name.ValueString()) {
-		return fmt.Errorf("app endpoint name %s", errors.ErrNotTrimmed)
-	}
-	// Validate endpoint name format
-	if !isValidEndpointName(plan.Name.ValueString()) {
-		return fmt.Errorf("app endpoint name must be between 1-100 characters and contain only lowercase letters, numbers, hyphens, and underscores")
-	}
-
-	// Validate userXattrKey if provided
-	if !plan.UserXattrKey.IsNull() && !plan.UserXattrKey.IsUnknown() {
-		if !providerschema.IsTrimmed(plan.UserXattrKey.ValueString()) {
-			return fmt.Errorf("userXattrKey %s", errors.ErrNotTrimmed)
-		}
-	}
-
-	return nil
-}
+//func (a *AppEndpoint) validateCreateAppEndpointRequest(plan providerschema.AppEndpoint) error {
+//	// Validate required IDs
+//	if plan.OrganizationId.IsNull() || plan.OrganizationId.IsUnknown() {
+//		return errors.ErrOrganizationIdCannotBeEmpty
+//	}
+//	if plan.ProjectId.IsNull() || plan.ProjectId.IsUnknown() {
+//		return errors.ErrProjectIdCannotBeEmpty
+//	}
+//	if plan.ClusterId.IsNull() || plan.ClusterId.IsUnknown() {
+//		return errors.ErrClusterIdCannotBeEmpty
+//	}
+//	if plan.AppServiceId.IsNull() || plan.AppServiceId.IsUnknown() {
+//		return errors.ErrAppServiceIdCannotBeEmpty
+//	}
+//
+//	// Validate required bucket name
+//	if plan.Bucket.IsNull() || plan.Bucket.IsUnknown() {
+//		return fmt.Errorf("bucket name cannot be empty")
+//	}
+//	if !providerschema.IsTrimmed(plan.Bucket.ValueString()) {
+//		return fmt.Errorf("bucket name %s", errors.ErrNotTrimmed)
+//	}
+//
+//	// Validate required endpoint name
+//	if plan.Name.IsNull() || plan.Name.IsUnknown() {
+//		return fmt.Errorf("app endpoint name cannot be empty")
+//	}
+//	if !providerschema.IsTrimmed(plan.Name.ValueString()) {
+//		return fmt.Errorf("app endpoint name %s", errors.ErrNotTrimmed)
+//	}
+//	// Validate endpoint name format
+//	if !isValidEndpointName(plan.Name.ValueString()) {
+//		return fmt.Errorf("app endpoint name must be between 1-100 characters and contain only lowercase letters, numbers, hyphens, and underscores")
+//	}
+//
+//	// Validate userXattrKey if provided
+//	if !plan.UserXattrKey.IsNull() && !plan.UserXattrKey.IsUnknown() {
+//		if !providerschema.IsTrimmed(plan.UserXattrKey.ValueString()) {
+//			return fmt.Errorf("userXattrKey %s", errors.ErrNotTrimmed)
+//		}
+//	}
+//
+//	// Validate OIDC configurations if provided
+//	if len(plan.Oidc) > 0 {
+//		for i, oidc := range plan.Oidc {
+//			if err := a.validateOidcConfiguration(oidc, i); err != nil {
+//				return err
+//			}
+//		}
+//	}
+//
+//	// Validate CORS configuration if provided
+//	if len(plan.Cors.Origin) > 0 {
+//		for i, origin := range plan.Cors.Origin {
+//			if !providerschema.IsTrimmed(origin.ValueString()) {
+//				return fmt.Errorf("cors origin at index %d %s", i, errors.ErrNotTrimmed)
+//			}
+//		}
+//	}
+//
+//	if len(plan.Cors.LoginOrigin) > 0 {
+//		for i, loginOrigin := range plan.Cors.LoginOrigin {
+//			if !providerschema.IsTrimmed(loginOrigin.ValueString()) {
+//				return fmt.Errorf("cors loginOrigin at index %d %s", i, errors.ErrNotTrimmed)
+//			}
+//		}
+//	}
+//
+//	if len(plan.Cors.Headers) > 0 {
+//		for i, header := range plan.Cors.Headers {
+//			if !providerschema.IsTrimmed(header.ValueString()) {
+//				return fmt.Errorf("cors header at index %d %s", i, errors.ErrNotTrimmed)
+//			}
+//		}
+//	}
+//
+//	// Validate CORS maxAge if provided
+//	if !plan.Cors.MaxAge.IsNull() && !plan.Cors.MaxAge.IsUnknown() {
+//		if plan.Cors.MaxAge.ValueInt64() < 0 {
+//			return fmt.Errorf("cors maxAge cannot be negative")
+//		}
+//	}
+//
+//	return nil
+//}
 
 // validateOidcConfiguration validates an individual OIDC configuration.
-func (a *AppEndpoint) validateOidcConfiguration(oidc providerschema.AppEndpointOidc, index int) error {
-	// Validate required issuer
-	if oidc.Issuer.IsNull() || oidc.Issuer.IsUnknown() {
-		return fmt.Errorf("oidc configuration at index %d: issuer cannot be empty", index)
-	}
-	if !providerschema.IsTrimmed(oidc.Issuer.ValueString()) {
-		return fmt.Errorf("oidc configuration at index %d: issuer %s", index, errors.ErrNotTrimmed)
-	}
-	// Validate issuer URL format
-	if !isValidURL(oidc.Issuer.ValueString()) {
-		return fmt.Errorf("oidc configuration at index %d: issuer must be a valid URL", index)
-	}
-
-	// Validate required clientId
-	if oidc.ClientId.IsNull() || oidc.ClientId.IsUnknown() {
-		return fmt.Errorf("oidc configuration at index %d: clientId cannot be empty", index)
-	}
-	if !providerschema.IsTrimmed(oidc.ClientId.ValueString()) {
-		return fmt.Errorf("oidc configuration at index %d: clientId %s", index, errors.ErrNotTrimmed)
-	}
-
-	// Validate optional fields if provided
-	if !oidc.UserPrefix.IsNull() && !oidc.UserPrefix.IsUnknown() {
-		if !providerschema.IsTrimmed(oidc.UserPrefix.ValueString()) {
-			return fmt.Errorf("oidc configuration at index %d: userPrefix %s", index, errors.ErrNotTrimmed)
-		}
-	}
-
-	if !oidc.DiscoveryUrl.IsNull() && !oidc.DiscoveryUrl.IsUnknown() {
-		if !providerschema.IsTrimmed(oidc.DiscoveryUrl.ValueString()) {
-			return fmt.Errorf("oidc configuration at index %d: discoveryUrl %s", index, errors.ErrNotTrimmed)
-		}
-		// Validate discovery URL format
-		if !isValidURL(oidc.DiscoveryUrl.ValueString()) {
-			return fmt.Errorf("oidc configuration at index %d: discoveryUrl must be a valid URL", index)
-		}
-	}
-
-	if !oidc.UsernameClaim.IsNull() && !oidc.UsernameClaim.IsUnknown() {
-		if !providerschema.IsTrimmed(oidc.UsernameClaim.ValueString()) {
-			return fmt.Errorf("oidc configuration at index %d: usernameClaim %s", index, errors.ErrNotTrimmed)
-		}
-	}
-
-	if !oidc.RolesClaim.IsNull() && !oidc.RolesClaim.IsUnknown() {
-		if !providerschema.IsTrimmed(oidc.RolesClaim.ValueString()) {
-			return fmt.Errorf("oidc configuration at index %d: rolesClaim %s", index, errors.ErrNotTrimmed)
-		}
-	}
-
-	return nil
-}
+//func (a *AppEndpoint) validateOidcConfiguration(oidc providerschema.AppEndpointOidc, index int) error {
+//	// Validate required issuer
+//	if oidc.Issuer.IsNull() || oidc.Issuer.IsUnknown() {
+//		return fmt.Errorf("oidc configuration at index %d: issuer cannot be empty", index)
+//	}
+//	if !providerschema.IsTrimmed(oidc.Issuer.ValueString()) {
+//		return fmt.Errorf("oidc configuration at index %d: issuer %s", index, errors.ErrNotTrimmed)
+//	}
+//	// Validate issuer URL format
+//	if !isValidURL(oidc.Issuer.ValueString()) {
+//		return fmt.Errorf("oidc configuration at index %d: issuer must be a valid URL", index)
+//	}
+//
+//	// Validate required clientId
+//	if oidc.ClientId.IsNull() || oidc.ClientId.IsUnknown() {
+//		return fmt.Errorf("oidc configuration at index %d: clientId cannot be empty", index)
+//	}
+//	if !providerschema.IsTrimmed(oidc.ClientId.ValueString()) {
+//		return fmt.Errorf("oidc configuration at index %d: clientId %s", index, errors.ErrNotTrimmed)
+//	}
+//
+//	// Validate optional fields if provided
+//	if !oidc.UserPrefix.IsNull() && !oidc.UserPrefix.IsUnknown() {
+//		if !providerschema.IsTrimmed(oidc.UserPrefix.ValueString()) {
+//			return fmt.Errorf("oidc configuration at index %d: userPrefix %s", index, errors.ErrNotTrimmed)
+//		}
+//	}
+//
+//	if !oidc.DiscoveryUrl.IsNull() && !oidc.DiscoveryUrl.IsUnknown() {
+//		if !providerschema.IsTrimmed(oidc.DiscoveryUrl.ValueString()) {
+//			return fmt.Errorf("oidc configuration at index %d: discoveryUrl %s", index, errors.ErrNotTrimmed)
+//		}
+//		// Validate discovery URL format
+//		if !isValidURL(oidc.DiscoveryUrl.ValueString()) {
+//			return fmt.Errorf("oidc configuration at index %d: discoveryUrl must be a valid URL", index)
+//		}
+//	}
+//
+//	if !oidc.UsernameClaim.IsNull() && !oidc.UsernameClaim.IsUnknown() {
+//		if !providerschema.IsTrimmed(oidc.UsernameClaim.ValueString()) {
+//			return fmt.Errorf("oidc configuration at index %d: usernameClaim %s", index, errors.ErrNotTrimmed)
+//		}
+//	}
+//
+//	if !oidc.RolesClaim.IsNull() && !oidc.RolesClaim.IsUnknown() {
+//		if !providerschema.IsTrimmed(oidc.RolesClaim.ValueString()) {
+//			return fmt.Errorf("oidc configuration at index %d: rolesClaim %s", index, errors.ErrNotTrimmed)
+//		}
+//	}
+//
+//	return nil
+//}
 
 // isValidURL checks if a string is a valid URL.
 func isValidURL(urlString string) bool {
@@ -292,19 +360,35 @@ func isValidEndpointName(name string) bool {
 	return true
 }
 
-func initializeAppEndpointWithPlan(plan providerschema.AppEndpoint) providerschema.AppEndpoint {
-	if plan.UserXattrKey.IsNull() || plan.UserXattrKey.IsUnknown() {
-		plan.UserXattrKey = types.StringNull()
+func initComputedAttributesToNull(plan providerschema.AppEndpoint) providerschema.AppEndpoint {
+	if plan.AdminURL.IsUnknown() || plan.AdminURL.IsNull() {
+		plan.AdminURL = types.StringNull()
 	}
-	if plan.DeltaSyncEnabled.IsNull() || plan.DeltaSyncEnabled.IsUnknown() {
-		plan.DeltaSyncEnabled = types.BoolNull()
+
+	if plan.PublicURL.IsUnknown() || plan.PublicURL.IsNull() {
+		plan.PublicURL = types.StringNull()
+
 	}
-	if plan.Cors.IsNull() || plan.Cors.IsUnknown() {
-		plan.Cors = types.ObjectNull(providerschema.AppEndpointCors{}.AttributeTypes())
+
+	if plan.MetricsURL.IsUnknown() || plan.MetricsURL.IsNull() {
+		plan.MetricsURL = types.StringNull()
 	}
-	if plan.Oidc.IsNull() || plan.Oidc.IsUnknown() {
-		plan.Oidc = types.ListNull(types.ObjectType{AttrTypes: providerschema.AppEndpointOidc{}.AttributeTypes()})
+
+	for i := range plan.Oidc {
+		if plan.Oidc[i].ProviderId.IsUnknown() || plan.Oidc[i].ProviderId.IsNull() {
+			plan.Oidc[i].ProviderId = types.StringNull()
+		}
+
+		if plan.Oidc[i].IsDefault.IsUnknown() || plan.Oidc[i].IsDefault.IsNull() {
+			plan.Oidc[i].IsDefault = types.BoolNull()
+		}
 	}
+
+	plan.RequireResync = types.MapNull(types.ObjectType{
+		AttrTypes: map[string]attr.Type{
+			"items": types.SetType{ElemType: types.StringType},
+		},
+	})
 
 	return plan
 }
@@ -313,7 +397,6 @@ func (a *AppEndpoint) Read(ctx context.Context, req resource.ReadRequest, resp *
 	// TODO: AV-104555: Implement read for App Endpoint
 }
 
-// Update allows to update the cluster to ON or OFF state.
 func (a *AppEndpoint) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	// TODO: AV-104552: Implement delete and update for App Endpoint
 }
