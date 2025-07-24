2cee2cfc486ad5f4926ffc2a9dd4fd4a75d5cc22
Hitesh Walia <hitesh.walia@couchbase.com>
2025-07-24 11:59:16 -0700

    update example

--- examples/app_endpoint/variables.tf
+++ examples/app_endpoint/variables.tf
@@ -1,3 +1,9 @@
+
+variable "auth_token" {
+  description = "Authentication API Key"
+  sensitive   = true
+}
+
 variable "organization_id" {
   description = "Capella Organization ID"
 }
@@ -14,46 +20,30 @@ variable "app_service_id" {
   description = "Capella App Service ID"
 }
 
-variable "auth_token" {
-  description = "Authentication API Key"
-  sensitive   = true
+variable "bucket" {
+  description = "bucket"
 }
 
-variable "app_endpoint" {
-  description = "App Endpoint configuration"
+variable "name" {
+  description = "name"
+}
 
-  type = object({
-    organization_id    = string
-    project_id         = string
-    cluster_id         = string
-    app_service_id     = string
-    bucket             = string
-    name               = string
-    user_xattr_key     = optional(string)
-    delta_sync_enabled = optional(bool)
-    scopes = optional(map(object({
-      collections = optional(map(object({
-        access_control_function = optional(string)
-        import_filter           = optional(string)
-      })))
-    })))
-    cors = optional(object({
-      origin      = optional(list(string))
-      login_origin = optional(list(string))
-      headers     = optional(list(string))
-      max_age     = optional(number)
-      disabled    = optional(bool)
-    }))
-    oidc = optional(list(object({
-      issuer         = string
-      register       = optional(bool)
-      client_id      = string
-      user_prefix    = optional(string)
-      discovery_url  = optional(string)
-      username_claim = optional(string)
-      roles_claim    = optional(string)
-      provider_id    = optional(string)
-      is_default     = optional(bool)
-    })))
-  })
+variable "delta_sync_enabled" {
+  description = "delta_sync_enabled"
+}
+
+variable "scope" {
+  description = "scope"
+}
+
+variable "collections" {
+  description = "collections"
+}
+
+variable "cors" {
+  description = "cors"
+}
+
+variable "oidc" {
+  description = "oidc"
 }
