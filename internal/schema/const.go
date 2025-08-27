ddfcdfb25d77c7cb08f457925ce3e4322fb06c30
Mohammed Madi <66013188+mohammed-madi@users.noreply.github.com>
2025-08-27 09:08:44 +0100

    [AV-104567] Add App endpoint activation status schema (#384)

--- internal/schema/const.go
+++ internal/schema/const.go
@@ -3,15 +3,16 @@ package schema
 type Attr string
 
 const (
-	OrganizationId Attr = "organizationId"
-	ProjectId      Attr = "projectId"
-	ClusterId      Attr = "clusterId"
-	BucketId       Attr = "bucketId"
-	BucketName     Attr = "bucketName"
-	Id             Attr = "id"
-	ScopeName      Attr = "scopeName"
-	CollectionName Attr = "collectionName"
-	AppServiceId   Attr = "appServiceId"
-	EndpointId     Attr = "endpointId"
-	IndexName      Attr = "indexName"
+	OrganizationId  Attr = "organizationId"
+	ProjectId       Attr = "projectId"
+	ClusterId       Attr = "clusterId"
+	BucketId        Attr = "bucketId"
+	BucketName      Attr = "bucketName"
+	Id              Attr = "id"
+	ScopeName       Attr = "scopeName"
+	CollectionName  Attr = "collectionName"
+	AppServiceId    Attr = "appServiceId"
+	AppEndpointName Attr = "appEndpointName"
+	EndpointId      Attr = "endpointId"
+	IndexName       Attr = "indexName"
 )
