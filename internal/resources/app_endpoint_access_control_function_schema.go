aa6c09730d4dfb45c991eae17cba7751844c040d
Hitesh Walia <hitesh.walia@couchbase.com>
2025-09-02 14:07:08 -0700

    resource

--- internal/resources/app_endpoint_access_control_function_schema.go
+++ internal/resources/app_endpoint_access_control_function_schema.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
 
-func AccessFunctionSchema() schema.Schema {
+func AccessControlFunctionSchema() schema.Schema {
 	return schema.Schema{
 		MarkdownDescription: "This Access Function resource allows you to manage access control and validation functions for App Endpoints in your Capella organization. Access functions are JavaScript functions that specify access control policies applied to documents in collections.",
 		Attributes: map[string]schema.Attribute{
@@ -12,7 +12,9 @@ func AccessFunctionSchema() schema.Schema {
 			"project_id":              WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the project."),
 			"cluster_id":              WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the cluster."),
 			"app_service_id":          WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the app service."),
-			"keyspace":                WithDescription(stringAttribute([]string{required, requiresReplace}), "The keyspace of the access function."),
+			"app_endpoint":            WithDescription(stringAttribute([]string{required, requiresReplace}), "The app endpoint name."),
+			"scope":                   WithDescription(stringDefaultAttribute("_default", optional, computed, requiresReplace), "The scope name of the access function."),
+			"collection":              WithDescription(stringDefaultAttribute("_default", optional, computed, requiresReplace), "The collection name of the access function."),
 			"access_control_function": WithDescription(stringAttribute([]string{required}), "The JavaScript function that is used to specify the access control policies to be applied to documents in this collection. Every document update is processed by this function."),
 		},
 	}
