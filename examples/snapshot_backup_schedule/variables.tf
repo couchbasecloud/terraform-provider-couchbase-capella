6404d6d04a4a23f4126f8901e40865faedca2b16
Sophie Wegmann <sophie.wegmann@couchbase.com>
2025-09-10 17:43:10 +0100

    Add acceptance tests and fix interval validation

--- examples/snapshot_backup_schedule/variables.tf
+++ examples/snapshot_backup_schedule/variables.tf
@@ -34,22 +34,24 @@ variable "snapshot_backup_schedule" {
   }
 
   validation {
-    condition     = var.snapshot_backup_schedule.interval >= 1 && var.snapshot_backup_schedule.interval <= 24
-    error_message = "Interval must be between 1 and 24 hours."
+    condition = anytrue([var.snapshot_backup_schedule.interval == null, 
+                          var.snapshot_backup_schedule.interval == 1, 
+                          var.snapshot_backup_schedule.interval == 2, 
+                          var.snapshot_backup_schedule.interval == 4, 
+                          var.snapshot_backup_schedule.interval == 6, 
+                          var.snapshot_backup_schedule.interval == 8, 
+                          var.snapshot_backup_schedule.interval == 12, 
+                          var.snapshot_backup_schedule.interval == 24])
+    error_message = "Interval must be 1, 2, 4, 6, 8, 12, or 24 hours."
   }
 
   validation {
-    condition = var.snapshot_backup_schedule.interval == floor(var.snapshot_backup_schedule.interval)
-    error_message = "Interval must be an integer."
-  }
-
-  validation {
-    condition = var.snapshot_backup_schedule.retention >= 24 && var.snapshot_backup_schedule.retention <= 720
+    condition = var.snapshot_backup_schedule.retention == null || (var.snapshot_backup_schedule.retention >= 24 && var.snapshot_backup_schedule.retention <= 720)
     error_message = "Retention must be between 24 and 720 hours."
   }
 
   validation {
-    condition = var.snapshot_backup_schedule.retention == floor(var.snapshot_backup_schedule.retention)
+    condition = var.snapshot_backup_schedule.retention == null || var.snapshot_backup_schedule.retention == floor(var.snapshot_backup_schedule.retention)
     error_message = "Retention must be an integer."
   }
 }
