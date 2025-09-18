e18da17e01d1859c8c527b73f1c18c682acd3535
Mohammed Madi <madim0044@gmail.com>
2025-09-18 10:19:43 +0100

    Change Descriptions to MarkdownDescritpions in schemas

--- internal/resources/app_endpoint_oidc_default_schema.go
+++ internal/resources/app_endpoint_oidc_default_schema.go
@@ -9,46 +9,46 @@ import (
 // AppEndpointDefaultOidcProviderSchema defines the schema for the default OIDC provider resource.
 func AppEndpointDefaultOidcProviderSchema() schema.Schema {
 	return schema.Schema{
-		MarkdownDescription: "Manage the default OIDC provider for an App Endpoint.",
+		MarkdownDescription: "Manage the default OpenID Connect provider for an App Endpoint.",
 		Attributes: map[string]schema.Attribute{
 			"organization_id": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
-				Description: "The GUID4 ID of the Capella Organization. Requires replacement if changed.",
+				MarkdownDescription: "The GUID4 ID of the Capella Organization. Requires replacement if changed.",
 			},
 			"project_id": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
-				Description: "The GUID4 ID of the Capella Project. Requires replacement if changed.",
+				MarkdownDescription: "The GUID4 ID of the Capella Project. Requires replacement if changed.",
 			},
 			"cluster_id": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
-				Description: "The GUID4 ID of the Capella Cluster. Requires replacement if changed.",
+				MarkdownDescription: "The GUID4 ID of the Capella Cluster. Requires replacement if changed.",
 			},
 			"app_service_id": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
-				Description: "The GUID4 ID of the App Service. Requires replacement if changed.",
+				MarkdownDescription: "The GUID4 ID of the App Service. Requires replacement if changed.",
 			},
 			"app_endpoint_name": schema.StringAttribute{
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplaceIfConfigured(),
 				},
-				Description: "The name of the App Endpoint. Requires replacement if changed.",
+				MarkdownDescription: "The name of the App Endpoint. Requires replacement if changed.",
 			},
 			"provider_id": schema.StringAttribute{
-				Required:    true,
-				Description: "The providerId to set as the default for this App Endpoint.",
+				Required:            true,
+				MarkdownDescription: "The providerId to set as the default for this App Endpoint.",
 			},
 		},
 	}
