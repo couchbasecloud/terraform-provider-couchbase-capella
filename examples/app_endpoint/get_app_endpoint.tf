cfc319f36538b89cb20c9ad923b2b429b6635d6d
Mohammed Madi <madim0044@gmail.com>
2025-10-01 16:48:57 +0100

    Add single app endpoint datasource to examples

--- examples/app_endpoint/get_app_endpoint.tf
+++ examples/app_endpoint/get_app_endpoint.tf
@@ -2,6 +2,10 @@ output "some_app_endpoints" {
   value = data.couchbase-capella_app_endpoints.filtered_app_endpoints
 }
 
+output "one_app_endpoint" {
+  value = data.couchbase-capella_app_endpoint.single_app_endpoint
+}
+
 data "couchbase-capella_app_endpoints" "filtered_app_endpoints" {
   organization_id = var.organization_id
   project_id      = couchbase-capella_project.new_project.id
@@ -14,4 +18,12 @@ data "couchbase-capella_app_endpoints" "filtered_app_endpoints" {
     name   = "name"
     values = ["app-endpoint-1"]
   }
+}
+
+data "couchbase-capella_app_endpoint" "single_app_endpoint" {
+  organization_id = var.organization_id
+  project_id      = couchbase-capella_project.new_project.id
+  cluster_id      = couchbase-capella_cluster.new_cluster.id
+  app_service_id = var.app_service.id
+  name = var.app_endpoint_name
 }
