eec85530ec53a4191a3e209cdbc9cf323220ecfb
Sophie Wegmann <sophie.wegmann@couchbase.com>
2025-10-23 16:59:10 +0100

    Implement restore datasource

---
+++ examples/data-sources/couchbase-capella_cloud_snapshot_restores/data-source.tf
@@ -0,0 +1,5 @@
+data "couchbase-capella_cloud_snapshot_restores" "existing_cloud_snapshot_restores" {
+  organization_id = "aaaaa-bbbb-cccc-dddd-eeee"
+  project_id      = "aaaaa-bbbb-cccc-dddd-eeee"
+  cluster_id      = "aaaaa-bbbb-cccc-dddd-eeee"
+}
