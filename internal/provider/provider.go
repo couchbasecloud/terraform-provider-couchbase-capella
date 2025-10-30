eed4c103fa301ef4c73c1602f7f72b30e483e4ea
SophieWegmann <sophie.wegmann@couchbase.com>
2025-10-30 09:25:36 +0000

    [AV-108538] Manage Cloud Snapshot Backup Schedule (#392)
    Allows cloud snapshot backup schedules to be created, read, updated, destroyed and imported. 
    Adds acceptance tests for the cloud snapshot backup schedule resource.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -288,5 +288,6 @@ func (p *capellaProvider) Resources(_ context.Context) []func() resource.Resourc
 		resources.NewAppEndpointOidcProvider,
 		resources.NewAppEndpointDefaultOidcProvider,
 		resources.NewSnapshotBackup,
+		resources.NewSnapshotBackupSchedule,
 	}
 }
