5cfa4c7b403bd96bc9f0abc0ca608781b656812c
SophieWegmann <sophie.wegmann@couchbase.com>
2025-11-04 09:47:57 +0000

    [AV-112221] Implement Cloud Backup Schedule Datasource (#425)

---
+++ examples/snapshot_backup_schedule/get_snapshot_backup_schedule/variables.tf
@@ -0,0 +1,34 @@
+variable "host" {
+  description = "Capella Host"
+}
+
+
+variable "organization_id" {
+  description = "Capella Organization ID"
+  type        = string
+}
+
+variable "auth_token" {
+  description = "Authentication API Key"
+  type        = string
+}
+
+variable "project_id" {
+  description = "Capella Project ID"
+  type        = string
+}
+
+variable "cluster_id" {
+  description = "Capella Cluster ID"
+  type        = string
+}
+
+variable "existing_cloud_snapshot_backup_schedule" {
+  description = "Existing snapshot backup schedule"
+  type        = object({
+    organization_id = string
+    project_id = string
+    cluster_id = string
+  })
+  default = null
+}
