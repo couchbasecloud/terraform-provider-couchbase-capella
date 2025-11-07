9513dcaebdb3e22bfa87b791b6539cfba35c2e0e
Talina Shrotriya <5362897+Talina06@users.noreply.github.com>
2025-11-07 15:09:19 -0800

    migrate app endpoints schemas

--- internal/resources/app_endpoint_oidc_default_schema.go
+++ internal/resources/app_endpoint_oidc_default_schema.go
@@ -4,25 +4,25 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	capellaschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 )
+
+var appEndpointOidcDefaultBuilder = capellaschema.NewSchemaBuilder("appEndpointOidcDefault")
 
 // AppEndpointDefaultOidcProviderSchema defines the schema for the default OIDC provider resource.
 func AppEndpointDefaultOidcProviderSchema() schema.Schema {
+	attrs := make(map[string]schema.Attribute)
+
+	capellaschema.AddAttr(attrs, "organization_id", appEndpointOidcDefaultBuilder, stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))))
+	capellaschema.AddAttr(attrs, "project_id", appEndpointOidcDefaultBuilder, stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))))
+	capellaschema.AddAttr(attrs, "cluster_id", appEndpointOidcDefaultBuilder, stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))))
+	capellaschema.AddAttr(attrs, "app_service_id", appEndpointOidcDefaultBuilder, stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))))
+	capellaschema.AddAttr(attrs, "app_endpoint_name", appEndpointOidcDefaultBuilder, stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))))
+	capellaschema.AddAttr(attrs, "provider_id", appEndpointOidcDefaultBuilder, stringAttribute([]string{required}, validator.String(stringvalidator.LengthAtLeast(1))))
+
 	return schema.Schema{
 		MarkdownDescription: "Manage the default OpenID Connect provider for an App Endpoint.",
-		Attributes: map[string]schema.Attribute{
-			"organization_id":   WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the Capella organization."),
-			"project_id":        WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the Capella project."),
-			"cluster_id":        WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the Capella cluster."),
-			"app_service_id":    WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The GUID4 ID of the Capella App Service."),
-			"app_endpoint_name": WithDescription(stringAttribute([]string{required, requiresReplace}, validator.String(stringvalidator.LengthAtLeast(1))), "The name of the App Endpoint."),
-			"provider_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The Id of the OpenID Connect provider to set as the default for this App Endpoint.",
-				Validators: []validator.String{
-					stringvalidator.LengthAtLeast(1),
-				},
-			},
-		},
+		Attributes:          attrs,
 	}
 }
