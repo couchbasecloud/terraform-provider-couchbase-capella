5962d24b923e78bd400b0604feb69490d29a0f21
Talina Shrotriya <5362897+Talina06@users.noreply.github.com>
2025-11-09 17:20:37 -0800

    [AV-113428] Migrate docs on resources to use the OpenAPI documentation [Part 2] (#434)

--- internal/resources/app_endpoint_access_control_function_schema.go
+++ internal/resources/app_endpoint_access_control_function_schema.go
@@ -2,20 +2,26 @@ package resources
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+
+	capellaschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 )
 
+var accessControlFunctionBuilder = capellaschema.NewSchemaBuilder("accessControlFunction")
+
 func AccessControlFunctionSchema() schema.Schema {
+	attrs := make(map[string]schema.Attribute)
+
+	capellaschema.AddAttr(attrs, "organization_id", accessControlFunctionBuilder, stringAttribute([]string{required, requiresReplace}))
+	capellaschema.AddAttr(attrs, "project_id", accessControlFunctionBuilder, stringAttribute([]string{required, requiresReplace}))
+	capellaschema.AddAttr(attrs, "cluster_id", accessControlFunctionBuilder, stringAttribute([]string{required, requiresReplace}))
+	capellaschema.AddAttr(attrs, "app_service_id", accessControlFunctionBuilder, stringAttribute([]string{required, requiresReplace}))
+	capellaschema.AddAttr(attrs, "app_endpoint_name", accessControlFunctionBuilder, stringAttribute([]string{required, requiresReplace}))
+	capellaschema.AddAttr(attrs, "scope", accessControlFunctionBuilder, stringDefaultAttribute("_default", optional, computed, requiresReplace))
+	capellaschema.AddAttr(attrs, "collection", accessControlFunctionBuilder, stringDefaultAttribute("_default", optional, computed, requiresReplace))
+	capellaschema.AddAttr(attrs, "access_control_function", accessControlFunctionBuilder, stringAttribute([]string{required}))
+
 	return schema.Schema{
 		MarkdownDescription: "This Access Function resource allows you to manage access control and validation functions for App Endpoints in your Capella organization. Access functions are JavaScript functions that specify access control policies applied to documents in collections.",
-		Attributes: map[string]schema.Attribute{
-			"organization_id":         WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the Couchbase Capella organization."),
-			"project_id":              WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the Couchbase Capella project."),
-			"cluster_id":              WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the Couchbase Capella Cluster."),
-			"app_service_id":          WithDescription(stringAttribute([]string{required, requiresReplace}), "The GUID4 ID of the Couchbase Capella App Service."),
-			"app_endpoint_name":       WithDescription(stringAttribute([]string{required, requiresReplace}), "The Couchbase Capella App endpoint name."),
-			"scope":                   WithDescription(stringDefaultAttribute("_default", optional, computed, requiresReplace), "The scope name of the keyspace (app_endpoint_name.scope.collection) containing documents to be processed with this access function."),
-			"collection":              WithDescription(stringDefaultAttribute("_default", optional, computed, requiresReplace), "The collection name of the keyspace (app_endpoint_name.scope.collection) containing documents to be processed with this access function."),
-			"access_control_function": WithDescription(stringAttribute([]string{required}), "The JavaScript function that is used to specify the access control policies to be applied to documents in this collection. Every document update is processed by this function."),
-		},
+		Attributes:          attrs,
 	}
 }
--- internal/resources/app_service_cidr_schema.go
+++ internal/resources/app_service_cidr_schema.go
@@ -4,71 +4,68 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+
+	capellaschema "github.com/couchbasecloud/terraform-provider-couchbase-capella/internal/schema"
 )
+
+var appServiceCIDRBuilder = capellaschema.NewSchemaBuilder("appServiceCIDR")
 
 // AllowedCIDRsSchema returns the schema for the App Service allowed CIDRs resource.
 func AllowedCIDRsSchema() schema.Schema {
+	attrs := make(map[string]schema.Attribute)
+
+	capellaschema.AddAttr(attrs, "id", appServiceCIDRBuilder, &schema.StringAttribute{
+		Computed: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.UseStateForUnknown(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "organization_id", appServiceCIDRBuilder, &schema.StringAttribute{
+		Required: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "project_id", appServiceCIDRBuilder, &schema.StringAttribute{
+		Required: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "cluster_id", appServiceCIDRBuilder, &schema.StringAttribute{
+		Required: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "app_service_id", appServiceCIDRBuilder, &schema.StringAttribute{
+		Required: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "cidr", appServiceCIDRBuilder, &schema.StringAttribute{
+		Required: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "comment", appServiceCIDRBuilder, &schema.StringAttribute{
+		Optional: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "expires_at", appServiceCIDRBuilder, &schema.StringAttribute{
+		Optional: true,
+		PlanModifiers: []planmodifier.String{
+			stringplanmodifier.RequiresReplace(),
+		},
+	})
+	capellaschema.AddAttr(attrs, "audit", appServiceCIDRBuilder, computedAuditAttribute())
+
 	return schema.Schema{
 		MarkdownDescription: "Manages the IP addresses allowed to connect to App Services in Couchbase Capella.",
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				MarkdownDescription: "The ID of the allowed CIDR block.",
-				Computed:            true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-			},
-			"organization_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The ID of the Capella organization.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"project_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The ID of the Capella project.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"cluster_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The ID of the Capella cluster.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"app_service_id": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The ID of the Capella App Service.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"cidr": schema.StringAttribute{
-				Required:            true,
-				MarkdownDescription: "The trusted CIDR block to allow the database connections from.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"comment": schema.StringAttribute{
-				Optional:            true,
-				MarkdownDescription: "A short description of the allowed CIDR block.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"expires_at": schema.StringAttribute{
-				Optional: true,
-				MarkdownDescription: "An RFC3339 timestamp determining when the allowed CIDR block will expire. " +
-					"If this field is omitted then the allowed CIDR block is permanent. It will never automatically expire.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
-			},
-			"audit": computedAuditAttribute(),
-		},
+		Attributes:          attrs,
 	}
 }
