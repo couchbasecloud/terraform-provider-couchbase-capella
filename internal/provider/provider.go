ead42d1e724847a30107642f3843e2572767fb1a
Mohammed Madi <madim0044@gmail.com>
2025-11-11 15:30:31 +0000

    Remove resync datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -240,7 +240,6 @@ func (p *capellaProvider) DataSources(_ context.Context) []func() datasource.Dat
 		datasources.NewFreeTierBuckets,
 		datasources.NewFreeTierClusters,
 		datasources.NewAppServiceCidrs,
-		datasources.NewAppEndpointResync,
 		datasources.NewAppEndpoints,
 		datasources.NewAppEndpoint,
 		datasources.NewAppEndpointActivationStatus,
