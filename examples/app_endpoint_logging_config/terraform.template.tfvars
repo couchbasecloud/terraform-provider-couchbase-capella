84113b920409ee02e8cdd6d472c5eb031865cedc
Sophie Wegmann <sophie.wegmann@couchbase.com>
2026-02-20 17:33:23 +0000

    Address comments

--- examples/app_endpoint_logging_config/terraform.template.tfvars
+++ examples/app_endpoint_logging_config/terraform.template.tfvars
@@ -6,10 +6,7 @@ cluster_id        = "<cluster_id>"
 app_service_id    = "<app_service_id>"
 app_endpoint_name = "<app_endpoint_name>"
 
-// log_level should be `info`, `warn` or `error`.
-// There should be at least one `log_key`.
-// Accepted log_keys are 'Admin', 'Access, 'Auth', 'Cache', 'Changes, 'CRUD', 'HTTP', 'HTTP+', 'Import', 'Javascript', 'Query', 'Sync' and 'SyncMsg'.
 app_endpoint_log_streaming_config = {
-  log_level = "<log_level>"
-  log_keys = ["<log_key_1>", "<log_key_2>"]
+  log_level = "info"
+  log_keys = ["Cache", "CRUD", "HTTP+", "Changes", "HTTP", "Query"]
 }
--- examples/app_endpoint_logging_config/variables.tf
+++ examples/app_endpoint_logging_config/variables.tf
@@ -35,34 +35,4 @@ variable "app_endpoint_log_streaming_config" {
     log_keys = set(string)
   })
 
-  validation {
-    condition = contains(["info", "warn", "error"], var.app_endpoint_log_streaming_config.log_level)
-    error_message = "log_level must be 'info', 'warn' or 'error'."
-  }
-
-  validation {
-    condition = length(var.app_endpoint_log_streaming_config.log_keys) >= 1
-    error_message = "There must be one or more log_keys."
-  }
-
-  validation {
-    condition = alltrue([
-      for log_key in var.app_endpoint_log_streaming_config.log_keys : contains([
-        "Admin",
-		    "Access",
-		    "Auth",
-		    "Cache",
-		    "Changes",
-		    "CRUD",
-		    "HTTP",
-		    "HTTP+",
-		    "Import",
-		    "Javascript",
-		    "Query",
-		    "Sync",
-		    "SyncMsg"], log_key)
-    ])
-    error_message = "log_key must be 'Admin', 'Access, 'Auth', 'Cache', 'Changes, 'CRUD', 'HTTP', 'HTTP+', 'Import', 'Javascript', 'Query', 'Sync' or 'SyncMsg'"
-  }
-
 }
