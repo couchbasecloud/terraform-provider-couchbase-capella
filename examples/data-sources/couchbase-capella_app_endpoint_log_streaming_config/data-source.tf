0374615abcbdda7ea70a889261f0cb1597238106
SophieWegmann <sophie.wegmann@couchbase.com>
2026-02-26 15:12:25 +0000

    [AV-121849] App Endpoint Logging Config Datasource Documentation (#490)

---
+++ examples/data-sources/couchbase-capella_app_endpoint_log_streaming_config/data-source.tf
@@ -0,0 +1,7 @@
+data "couchbase-capella_app_endpoint_log_streaming_config" "app_endpoint_log_streaming_config" {
+  organization_id  = "aaaaa-bbbb-cccc-dddd-eeee"
+  project_id = "aaaaa-bbbb-cccc-dddd-eeee"
+  cluster_id = "aaaaa-bbbb-cccc-dddd-eeee"
+  app_service_id = "aaaaa-bbbb-cccc-dddd-eeee"
+  app_endpoint_name = "app_endpoint_name"
+}
