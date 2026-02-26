a231ee74808b9ea0477ab9085295d4a088f395c2
Isaac Lambat <isaac.lambat@couchbase.com>
2026-02-26 11:32:17 +0000

    AV-123608: Use types.Object for credentials

--- examples/app_service_log_streaming/variables.tf
+++ examples/app_service_log_streaming/variables.tf
@@ -21,11 +21,42 @@ variable "app_service_id" {
 
 variable "output_type" {
   description = "Type of Log Collector to stream logs to"
-  type = string
+  type        = string
 }
 
 variable "credentials" {
   description = "Credentials for the Log Collector to stream logs to. The structure of this variable will depend on the output_type specified."
-  type = map(any)
   sensitive   = true
+  type = object({
+    datadog = optional(object({
+      url     = string
+      api_key = string
+    }))
+    dynatrace = optional(object({
+      url       = string
+      api_token = string
+    }))
+    elastic = optional(object({
+      url      = string
+      user     = string
+      password = string
+    }))
+    generic_http = optional(object({
+      url      = string
+      user     = optional(string)
+      password = optional(string)
+    }))
+    loki = optional(object({
+      url      = string
+      user     = string
+      password = string
+    }))
+    splunk = optional(object({
+      url          = string
+      splunk_token = string
+    }))
+    sumologic = optional(object({
+      url = string
+    }))
+  })
 }
