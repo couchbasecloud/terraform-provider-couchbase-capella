56ea9e9d24b57584065facdd109e37972947fe55
SophieWegmann <sophie.wegmann@couchbase.com>
2026-03-09 10:25:22 +0000

    [AV-121438] App Endpoint Resync Data Source Implementation (#508)

--- internal/datasources/app_endpoint_resync_schema.go
+++ internal/datasources/app_endpoint_resync_schema.go
@@ -13,11 +13,11 @@ var appEndpointResyncBuilder = capellaschema.NewSchemaBuilder("appEndpointResync
 func AppEndpointResyncSchema() schema.Schema {
 	attrs := make(map[string]schema.Attribute)
 
-	capellaschema.AddAttr(attrs, "organization_id", appEndpointResyncBuilder, requiredString())
-	capellaschema.AddAttr(attrs, "project_id", appEndpointResyncBuilder, requiredString())
-	capellaschema.AddAttr(attrs, "cluster_id", appEndpointResyncBuilder, requiredString())
-	capellaschema.AddAttr(attrs, "app_service_id", appEndpointResyncBuilder, requiredString())
-	capellaschema.AddAttr(attrs, "app_endpoint_name", appEndpointResyncBuilder, requiredString())
+	capellaschema.AddAttr(attrs, "organization_id", appEndpointResyncBuilder, requiredStringWithValidator())
+	capellaschema.AddAttr(attrs, "project_id", appEndpointResyncBuilder, requiredStringWithValidator())
+	capellaschema.AddAttr(attrs, "cluster_id", appEndpointResyncBuilder, requiredStringWithValidator())
+	capellaschema.AddAttr(attrs, "app_service_id", appEndpointResyncBuilder, requiredStringWithValidator())
+	capellaschema.AddAttr(attrs, "app_endpoint_name", appEndpointResyncBuilder, requiredStringWithValidator())
 	capellaschema.AddAttr(attrs, "collections_processing", appEndpointResyncBuilder, &schema.MapAttribute{
 		ElementType: types.SetType{
 			ElemType: types.StringType,
