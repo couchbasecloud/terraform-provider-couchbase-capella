84109ce4f287746c17b535e98be561c1f9aad002
Hitesh Walia <hitesh.walia@couchbase.com>
2026-04-07 14:33:23 -0700

    update api schema

--- internal/api/private_endpoints.go
+++ internal/api/private_endpoints.go
@@ -4,13 +4,16 @@ package api
 type GetPrivateEndpointResponse struct {
 	// Id is the endpoint id.
 	Id string `json:"id"`
-	// Status is the endpoint status.  Possible values are failed, linked, pending, pendingAcceptance, rejected and unrecognized.
+
+	// Status is the endpoint status.  Possible values are failed, linked, pending, pendingAcceptance, rejected and unrecognized
 	Status string `json:"status"`
+
 	// ServiceName is the name of the endpoint service.
 	ServiceName string `json:"serviceName"`
 }
 
 // GetPrivateEndpointsResponse is a list of private endpoints.
 type GetPrivateEndpointsResponse struct {
-	Endpoints []GetPrivateEndpointResponse `json:"endpoints"`
+	PrivateEndpointDNS string                       `json:"privateEndpointDNS"`
+	Endpoints          []GetPrivateEndpointResponse `json:"endpoints"`
 }
