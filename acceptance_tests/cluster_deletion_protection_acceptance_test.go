a07735f25d6f79b1c5e68ad14601b98a10fb5ffb
Nimiya Joseph <nimiya.joseph@couchbase.com>
2026-05-27 09:58:51 +0530

    test: add deletion protection acceptance coverage

--- acceptance_tests/cluster_deletion_protection_acceptance_test.go
+++ acceptance_tests/cluster_deletion_protection_acceptance_test.go
@@ -78,6 +78,143 @@ func TestAccClusterDeletionProtectionInvalidCluster(t *testing.T) {
 	})
 }
 
+func TestAccClusterDeletionProtectionProtectedDestroyFails(t *testing.T) {
+	clusterResourceName := randomStringWithPrefix("tf_acc_cluster_del_prot_")
+	deletionProtectionResourceName := randomStringWithPrefix("tf_acc_del_prot_destroy_")
+	clusterReference := "couchbase-capella_cluster." + clusterResourceName
+	deletionProtectionReference := "couchbase-capella_cluster_deletion_protection." + deletionProtectionResourceName
+	cidr := generateRandomCIDR()
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: globalProtoV6ProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccClusterDeletionProtectionProtectedClusterConfig(clusterResourceName, deletionProtectionResourceName, cidr, true),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "name", clusterResourceName),
+					resource.TestCheckResourceAttrSet(clusterReference, "id"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "true"),
+				),
+			},
+			{
+				Config:      testAccClusterDeletionProtectionProtectedClusterConfig(clusterResourceName, deletionProtectionResourceName, cidr, true),
+				Destroy:     true,
+				ExpectError: regexp.MustCompile("Cluster deletion protection is enabled"),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedClusterConfig(clusterResourceName, deletionProtectionResourceName, cidr, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedClusterConfig(clusterResourceName, deletionProtectionResourceName, cidr, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "deletion_protection", "false"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccClusterDeletionProtectionProtectedBucketDeleteFails(t *testing.T) {
+	clusterResourceName := randomStringWithPrefix("tf_acc_cluster_del_prot_bucket_")
+	bucketResourceName := randomStringWithPrefix("tf_acc_bucket_del_prot_")
+	deletionProtectionResourceName := randomStringWithPrefix("tf_acc_del_prot_bucket_")
+	clusterReference := "couchbase-capella_cluster." + clusterResourceName
+	bucketReference := "couchbase-capella_bucket." + bucketResourceName
+	deletionProtectionReference := "couchbase-capella_cluster_deletion_protection." + deletionProtectionResourceName
+	cidr := generateRandomCIDR()
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: globalProtoV6ProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketConfig(clusterResourceName, bucketResourceName, deletionProtectionResourceName, cidr, true, true),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "name", clusterResourceName),
+					resource.TestCheckResourceAttr(bucketReference, "name", bucketResourceName),
+					resource.TestCheckResourceAttrSet(bucketReference, "id"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "true"),
+				),
+			},
+			{
+				Config:      testAccClusterDeletionProtectionProtectedBucketConfig(clusterResourceName, bucketResourceName, deletionProtectionResourceName, cidr, true, false),
+				ExpectError: regexp.MustCompile(`(?s)Unable to delete bucket.*disable cluster\s+deletion protection`),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketConfig(clusterResourceName, bucketResourceName, deletionProtectionResourceName, cidr, false, true),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(bucketReference, "name", bucketResourceName),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketConfig(clusterResourceName, bucketResourceName, deletionProtectionResourceName, cidr, false, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "deletion_protection", "false"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccClusterDeletionProtectionProtectedBucketFlushFails(t *testing.T) {
+	clusterResourceName := randomStringWithPrefix("tf_acc_cluster_del_prot_flush_")
+	bucketResourceName := randomStringWithPrefix("tf_acc_bucket_flush_prot_")
+	flushResourceName := randomStringWithPrefix("tf_acc_flush_del_prot_")
+	deletionProtectionResourceName := randomStringWithPrefix("tf_acc_del_prot_flush_")
+	clusterReference := "couchbase-capella_cluster." + clusterResourceName
+	bucketReference := "couchbase-capella_bucket." + bucketResourceName
+	deletionProtectionReference := "couchbase-capella_cluster_deletion_protection." + deletionProtectionResourceName
+	cidr := generateRandomCIDR()
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: globalProtoV6ProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketFlushConfig(clusterResourceName, bucketResourceName, flushResourceName, deletionProtectionResourceName, cidr, true, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "name", clusterResourceName),
+					resource.TestCheckResourceAttr(bucketReference, "name", bucketResourceName),
+					resource.TestCheckResourceAttr(bucketReference, "flush", "true"),
+					resource.TestCheckResourceAttrSet(bucketReference, "id"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "true"),
+				),
+			},
+			{
+				Config:      testAccClusterDeletionProtectionProtectedBucketFlushConfig(clusterResourceName, bucketResourceName, flushResourceName, deletionProtectionResourceName, cidr, true, true),
+				ExpectError: regexp.MustCompile(`(?is)Error flushing the bucket.*deletion.*protection`),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketFlushConfig(clusterResourceName, bucketResourceName, flushResourceName, deletionProtectionResourceName, cidr, false, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(bucketReference, "name", bucketResourceName),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+			{
+				Config: testAccClusterDeletionProtectionProtectedBucketFlushConfig(clusterResourceName, bucketResourceName, flushResourceName, deletionProtectionResourceName, cidr, false, false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccExistsClusterResource(t, clusterReference),
+					resource.TestCheckResourceAttr(clusterReference, "deletion_protection", "false"),
+					resource.TestCheckResourceAttr(deletionProtectionReference, "deletion_protection", "false"),
+				),
+			},
+		},
+	})
+}
+
 // --- Config builders ---
 
 func testAccClusterDeletionProtectionConfig(resourceName, clusterID string, enabled bool) string {
@@ -91,6 +228,197 @@ resource "couchbase-capella_cluster_deletion_protection" "%[2]s" {
   deletion_protection = %[6]t
 }
 `, globalProviderBlock, resourceName, globalOrgId, globalProjectId, clusterID, enabled)
+}
+
+func testAccClusterDeletionProtectionProtectedClusterConfig(clusterResourceName, deletionProtectionResourceName, cidr string, enabled bool) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "couchbase-capella_cluster" "%[4]s" {
+	organization_id = "%[2]s"
+	project_id      = "%[3]s"
+	name            = "%[4]s"
+	cloud_provider = {
+		type   = "aws"
+		region = "us-east-1"
+		cidr   = "%[6]s"
+	}
+	service_groups = [
+		{
+			node = {
+				compute = {
+					cpu = 4
+					ram = 16
+				}
+				disk = {
+					storage = 50
+					type    = "gp3"
+					iops    = 3000
+				}
+			}
+			num_of_nodes = 3
+			services     = ["data", "index", "query"]
+		}
+	]
+	availability = {
+		"type" : "multi"
+	}
+	support = {
+		plan     = "developer pro"
+		timezone = "PT"
+	}
+}
+
+resource "couchbase-capella_cluster_deletion_protection" "%[5]s" {
+	organization_id     = "%[2]s"
+	project_id          = "%[3]s"
+	cluster_id          = couchbase-capella_cluster.%[4]s.id
+	deletion_protection = %[7]t
+
+	depends_on = [couchbase-capella_cluster.%[4]s]
+}
+`, globalProviderBlock, globalOrgId, globalProjectId, clusterResourceName, deletionProtectionResourceName, cidr, enabled)
+}
+
+func testAccClusterDeletionProtectionProtectedBucketConfig(clusterResourceName, bucketResourceName, deletionProtectionResourceName, cidr string, enabled, includeBucket bool) string {
+	bucketConfig := ""
+	if includeBucket {
+		bucketConfig = fmt.Sprintf(`
+resource "couchbase-capella_bucket" %[1]q {
+	organization_id = %[2]q
+	project_id      = %[3]q
+	cluster_id      = couchbase-capella_cluster.%[4]s.id
+	name            = %[1]q
+
+	depends_on = [couchbase-capella_cluster.%[4]s]
+}
+`, bucketResourceName, globalOrgId, globalProjectId, clusterResourceName)
+	}
+
+	return fmt.Sprintf(`
+%[1]s
+
+resource "couchbase-capella_cluster" "%[4]s" {
+	organization_id = "%[2]s"
+	project_id      = "%[3]s"
+	name            = "%[4]s"
+	cloud_provider = {
+		type   = "aws"
+		region = "us-east-1"
+		cidr   = "%[7]s"
+	}
+	service_groups = [
+		{
+			node = {
+				compute = {
+					cpu = 4
+					ram = 16
+				}
+				disk = {
+					storage = 50
+					type    = "gp3"
+					iops    = 3000
+				}
+			}
+			num_of_nodes = 3
+			services     = ["data", "index", "query"]
+		}
+	]
+	availability = {
+		"type" : "multi"
+	}
+	support = {
+		plan     = "developer pro"
+		timezone = "PT"
+	}
+}
+
+%[8]s
+resource "couchbase-capella_cluster_deletion_protection" "%[6]s" {
+	organization_id     = "%[2]s"
+	project_id          = "%[3]s"
+	cluster_id          = couchbase-capella_cluster.%[4]s.id
+	deletion_protection = %[5]t
+
+	depends_on = [couchbase-capella_cluster.%[4]s]
+}
+`, globalProviderBlock, globalOrgId, globalProjectId, clusterResourceName, enabled, deletionProtectionResourceName, cidr, bucketConfig)
+}
+
+func testAccClusterDeletionProtectionProtectedBucketFlushConfig(clusterResourceName, bucketResourceName, flushResourceName, deletionProtectionResourceName, cidr string, enabled, includeFlush bool) string {
+	flushConfig := ""
+	if includeFlush {
+		flushConfig = fmt.Sprintf(`
+resource "couchbase-capella_flush" %[1]q {
+	organization_id = %[2]q
+	project_id      = %[3]q
+	cluster_id      = couchbase-capella_cluster.%[4]s.id
+	bucket_id       = couchbase-capella_bucket.%[5]s.id
+
+	depends_on = [couchbase-capella_cluster_deletion_protection.%[6]s]
+}
+`, flushResourceName, globalOrgId, globalProjectId, clusterResourceName, bucketResourceName, deletionProtectionResourceName)
+	}
+
+	return fmt.Sprintf(`
+%[1]s
+
+resource "couchbase-capella_cluster" "%[4]s" {
+	organization_id = "%[2]s"
+	project_id      = "%[3]s"
+	name            = "%[4]s"
+	cloud_provider = {
+		type   = "aws"
+		region = "us-east-1"
+		cidr   = "%[8]s"
+	}
+	service_groups = [
+		{
+			node = {
+				compute = {
+					cpu = 4
+					ram = 16
+				}
+				disk = {
+					storage = 50
+					type    = "gp3"
+					iops    = 3000
+				}
+			}
+			num_of_nodes = 3
+			services     = ["data", "index", "query"]
+		}
+	]
+	availability = {
+		"type" : "multi"
+	}
+	support = {
+		plan     = "developer pro"
+		timezone = "PT"
+	}
+}
+
+resource "couchbase-capella_bucket" "%[5]s" {
+	organization_id = "%[2]s"
+	project_id      = "%[3]s"
+	cluster_id      = couchbase-capella_cluster.%[4]s.id
+	name            = "%[5]s"
+	flush           = true
+
+	depends_on = [couchbase-capella_cluster.%[4]s]
+}
+
+resource "couchbase-capella_cluster_deletion_protection" "%[7]s" {
+	organization_id     = "%[2]s"
+	project_id          = "%[3]s"
+	cluster_id          = couchbase-capella_cluster.%[4]s.id
+	deletion_protection = %[6]t
+
+	depends_on = [couchbase-capella_bucket.%[5]s]
+}
+
+%[9]s
+`, globalProviderBlock, globalOrgId, globalProjectId, clusterResourceName, bucketResourceName, enabled, deletionProtectionResourceName, cidr, flushConfig)
 }
 
 // --- Import ID ---
