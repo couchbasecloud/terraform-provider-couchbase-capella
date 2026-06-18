a82153c4ab80b698dd6eac2f2d1c7faf734f36a5
Stanley Pearson <stanley.pearson@couchbase.com>
2026-06-18 19:01:47 +0530

    [AV-129960] review comment

--- internal/generated/api/retry_http_client.go
+++ internal/generated/api/retry_http_client.go
@@ -57,11 +57,8 @@ func customRetryPolicy(ctx context.Context, resp *http.Response, err error) (boo
 	}
 
 	switch resp.StatusCode {
-	case http.StatusTooManyRequests:
+	case http.StatusTooManyRequests, http.StatusServiceUnavailable:
 		// Always retry 429 responses - retryablehttp handles Retry-After header automatically
-		return true, nil
-
-	case http.StatusServiceUnavailable:
 		// Retry 503 responses - these indicate transient conditions such as a bucket
 		// deletion in progress that prevents creation of another bucket.
 		return true, nil
