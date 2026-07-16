7328a98f81a8401ee4af7effc560db6fe62a24d1
Stanley Pearson <stanley.pearson@couchbase.com>
2026-07-16 16:09:14 +0530

    [AV-127967] Update existing database credential acceptance tests in Terraform

--- acceptance_tests/database_credentials_datasource_test.go
+++ acceptance_tests/database_credentials_datasource_test.go
@@ -39,6 +39,37 @@ func TestAccDatasourceDatabaseCredentials(t *testing.T) {
 	})
 }
 
+// TestAccDatasourceDatabaseCredentialsAdvanced verifies that the datasource returns the
+// user roles assigned to an advanced database credential.
+func TestAccDatasourceDatabaseCredentialsAdvanced(t *testing.T) {
+	resourceName := randomStringWithPrefix("tf_acc_database_credentials_adv_")
+	dsName := randomStringWithPrefix("tf_acc_database_credentials_ds_")
+	roleName := randomStringWithPrefix("tf_acc_db_role_")
+	resourceReference := "couchbase-capella_database_credential." + resourceName
+	dsReference := "data.couchbase-capella_database_credentials." + dsName
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: globalProtoV6ProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDatabaseCredentialsAdvancedDataSourceConfig(resourceName, dsName, roleName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceReference, "name", resourceName),
+					resource.TestCheckResourceAttr(resourceReference, "credential_type", "advanced"),
+					resource.TestCheckResourceAttrSet(resourceReference, "id"),
+
+					resource.TestCheckResourceAttrSet(dsReference, "data.#"),
+					resource.TestCheckTypeSetElemNestedAttrs(dsReference, "data.*", map[string]string{
+						"name":         resourceName,
+						"user_roles.#": "1",
+						"user_roles.0": roleName,
+					}),
+				),
+			},
+		},
+	})
+}
+
 func TestAccDatasourceDatabaseCredentialsInvalidCluster(t *testing.T) {
 	dsName := randomStringWithPrefix("tf_acc_database_credentials_ds_invalid_")
 
@@ -86,4 +117,36 @@ data "couchbase-capella_database_credentials" "%[6]s" {
   depends_on = [couchbase-capella_database_credential.%[5]s]
 }
 `, globalProviderBlock, globalOrgId, globalProjectId, globalClusterId, resourceName, dsName)
+}
+
+func testAccDatabaseCredentialsAdvancedDataSourceConfig(resourceName, dsName, roleName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+resource "couchbase-capella_database_role" "role1" {
+  organization_id = "%[2]s"
+  project_id      = "%[3]s"
+  cluster_id      = "%[4]s"
+  name            = "%[7]s"
+  %[8]s
+}
+
+resource "couchbase-capella_database_credential" "%[5]s" {
+  name            = "%[5]s"
+  organization_id = "%[2]s"
+  project_id      = "%[3]s"
+  cluster_id      = "%[4]s"
+  credential_type = "advanced"
+  user_roles      = [couchbase-capella_database_role.role1.name]
+}
+
+data "couchbase-capella_database_credentials" "%[6]s" {
+  organization_id = "%[2]s"
+  project_id      = "%[3]s"
+  cluster_id      = "%[4]s"
+
+  depends_on = [couchbase-capella_database_credential.%[5]s]
+}
+`, globalProviderBlock, globalOrgId, globalProjectId, globalClusterId, resourceName, dsName,
+		roleName, databaseRoleAccessBlock(`"dataRead"`))
 }
